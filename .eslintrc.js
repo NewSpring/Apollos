bb479a88553c90d1e4a2cd9c21b720ece373f67b
Marcelo Reyna <lepozepo@gmail.com>
2017-10-10 10:51:10 -0700

    Patch linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,5 @@
 module.exports = {
+  "parser": "babel-eslint",
   "extends": ["airbnb"],
   "plugins": [
     "jest",
@@ -7,10 +8,16 @@ module.exports = {
     "import/no-extraneous-dependencies": ["error", { "devDependencies": true }],
     "react/jsx-filename-extension": 0,
     "function-paren-newline": 0,
+    "import/no-named-as-default": 0,
+    "import/no-named-as-default-member": 0,
   },
   "env": {
     "jest/globals": true,
     "browser": true,
     "node": true,
+  },
+  "ecmaFeatures": {
+    "classes": true,
+    "jsx": true
   },
 }
