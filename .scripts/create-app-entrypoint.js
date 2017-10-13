71756aec54eee9657d88b0708f1498129d511818
Marcelo Reyna <Lepozepo@gmail.com>
2017-10-13 11:09:47 -0700

    22 initial start bug (#23)
    * Remove App.js
    
    * Programatically create App.js on run
    
    * Ignore App.js
    
    * Remove REACT_NATIVE_IS_STORYBOOK env var

---
+++ .scripts/create-app-entrypoint.js
@@ -0,0 +1,15 @@
+const Path = require('path');
+const FS = require('fs');
+
+const content = `
+// create-react-native-app requires App.js
+import Src from './src';
+
+export default Src;
+`;
+
+try {
+  FS.writeFileSync(Path.resolve(__dirname, '../App.js'), content);
+} catch (err) {
+  console.error(err);
+}
---
+++ .scripts/create-storybook-entrypoint.js
@@ -0,0 +1,15 @@
+const Path = require('path');
+const FS = require('fs');
+
+const content = `
+// create-react-native-app requires App.js
+import StorybookUI from './src/.storybookNative';
+
+export default StorybookUI;
+`;
+
+try {
+  FS.writeFileSync(Path.resolve(__dirname, '../App.js'), content);
+} catch (err) {
+  console.error(err);
+}
