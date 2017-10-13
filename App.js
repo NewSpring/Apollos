58ac1f51162e681677ebf8dc8a456b916f12f03a
Marcelo Reyna <lepozepo@gmail.com>
2017-10-13 10:27:09 -0700

    Remove App.js

--- App.js
+++
@@ -1,5 +0,0 @@
-// create-react-native-app requires App.js
-import Src from './src';
-import StorybookUI from './src/.storybookNative';
-
-module.exports = process.env.REACT_NATIVE_IS_STORYBOOK ? StorybookUI : Src;
