75aeac1c55dab2dd2124429fff43e5b5cd8b8327
Marcelo Reyna <lepozepo@gmail.com>
2017-10-19 07:49:26 -0700

    Attach platform prefixes to settings keys (shallow)

--- .scripts/run-with-settings.js
+++ .scripts/run-with-settings.js
@@ -1,9 +1,18 @@
 const Fs = require('fs');
 const { spawn } = require('child_process');
 const Path = require('path');
+const mapKeys = require('lodash/mapKeys');
 
 const rawConfig = Fs.readFileSync(Path.resolve(__dirname, '../settings.json'), 'utf8');
 const config = JSON.parse(rawConfig);
+const platformConfigs = {
+  native: mapKeys(config, (v, k) => (`REACT_NATIVE_${k}`)),
+  web: mapKeys(config, (v, k) => (`REACT_APP_${k}`)),
+};
+const mappedConfig = {
+  ...platformConfigs.native,
+  ...platformConfigs.web,
+};
 
 const args = process.argv.slice(2);
 
@@ -14,6 +23,6 @@ spawn(command, {
   shell: true,
   env: {
     ...process.env,
-    ...config,
+    ...mappedConfig,
   },
 });
