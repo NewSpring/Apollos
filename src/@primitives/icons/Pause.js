ee119ac87676c96cd2da88035bc51288099afce0
Marcelo Reyna <lepozepo@gmail.com>
2017-10-23 09:01:10 -0700

    Use default theme's primary color on icon fills

--- src/@primitives/icons/Pause.js
+++ src/@primitives/icons/Pause.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Svg } from '../Svg';
+import { DEFAULT_THEME } from '../constants';
 
-export default ({ size = 32, fill = 'red' } = {}) => (
+export default ({ size = 32, fill = DEFAULT_THEME.primaryColor } = {}) => (
   <Svg width={size} height={size} viewBox="0 0 512 512">
     <Svg.Path
       d="M137 512c12 0 22-10 22-23l0-455c0-12-10-23-22-23-13 0-23 11-23 23l0 455c0 13 10 23 23 23z m227 0c13 0 23-10 23-23l0-455c0-12-10-23-23-23-12 0-23 11-23 23l0 455c0 13 11 23 23 23z"
--- src/@primitives/icons/Play.js
+++ src/@primitives/icons/Play.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Svg } from '../Svg';
+import { DEFAULT_THEME } from '../constants';
 
-export default ({ size = 32, fill = 'red' } = {}) => (
+export default ({ size = 32, fill = DEFAULT_THEME.primaryColor } = {}) => (
   <Svg width={size} height={size} viewBox="0 0 512 512">
     <Svg.Path
       d="M125 478l0-441c0-3 1-4 1-4-2 3-5 6-9 8-5 2-9 3-12 2 0 0 1 1 3 2l274 214c5 4 5 5 0 9l-272 202c-2 2-4 2-4 2 3-1 7 0 12 2 4 2 6 5 8 8 0 0-1-1-1-4z m-40 0c0 33 22 44 49 24l272-202c26-19 27-52 1-72l-274-214c-26-21-48-11-48 23z"
