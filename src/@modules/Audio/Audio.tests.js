53113315be3a5eb72d35bc15a611f524cb62aee9
Marcelo Reyna <lepozepo@gmail.com>
2017-10-23 10:46:18 -0700

    Build broken Audio component test
    SVGs don’t work with renderer, they need a mock

---
+++ src/@modules/Audio/Audio.tests.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import ThemeProvider from '../../@primitives/ThemeProvider';
+import Audio from './';
+
+it('renders correctly', () => {
+  const tree = renderer.create(
+    <ThemeProvider>
+      <Audio source="https://www.w3schools.com/html/horse.mp3" />
+    </ThemeProvider>,
+  );
+  expect(tree).toMatchSnapshot();
+});
