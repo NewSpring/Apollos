8c141f5810655f25533a6fdd7985d840309afd4b
Marcelo Reyna <lepozepo@gmail.com>
2017-10-23 13:58:18 -0700

    Make sure the file exists before attempting to read it

--- .scripts/run-with-settings.js
+++ .scripts/run-with-settings.js
@@ -4,16 +4,23 @@ const Path = require('path');
 const mapKeys = require('lodash/mapKeys');
 const dotenv = require('dotenv');
 
-const rawConfig = Fs.readFileSync(Path.resolve(__dirname, '../.env'), 'utf8');
-const config = dotenv.parse(rawConfig);
-const platformConfigs = {
-  native: mapKeys(config, (v, k) => (`REACT_NATIVE_${k}`)),
-  web: mapKeys(config, (v, k) => (`REACT_APP_${k}`)),
-};
-const mappedConfig = {
-  ...platformConfigs.native,
-  ...platformConfigs.web,
-};
+const envFile = '../.env';
+const fileExists = Fs.existsSync(envFile);
+
+let mappedConfig = {};
+
+if (fileExists) {
+  const rawConfig = Fs.readFileSync(Path.resolve(__dirname, envFile), 'utf8');
+  const config = dotenv.parse(rawConfig);
+  const platformConfigs = {
+    native: mapKeys(config, (v, k) => (`REACT_NATIVE_${k}`)),
+    web: mapKeys(config, (v, k) => (`REACT_APP_${k}`)),
+  };
+  mappedConfig = {
+    ...platformConfigs.native,
+    ...platformConfigs.web,
+  };
+}
 
 const args = process.argv.slice(2);
 
