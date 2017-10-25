f94f03e95c19e5142b74f9d465251d77f7a9663d
Marcelo Reyna <Lepozepo@gmail.com>
2017-10-25 13:50:56 -0700

    Build AudioSeeker

---
+++ src/@modules/Audio/AudioSeeker.js
@@ -0,0 +1,50 @@
+import React, { PureComponent } from 'react';
+import getContext from 'recompose/getContext';
+import compose from 'recompose/compose';
+import PropTypes from 'prop-types';
+import Seeker from '../../@primitives/Seeker';
+
+export class AudioSeeker extends PureComponent {
+  static propTypes = {
+    seek: PropTypes.func.isRequired,
+    seekingHandler: PropTypes.func,
+    progress: PropTypes.number,
+    component: PropTypes.oneOfType([
+      PropTypes.element,
+      PropTypes.func,
+    ]),
+  };
+
+  static defaultProps = {
+    seekingHandler() {},
+    progress: 0,
+    component: Seeker,
+  };
+
+  render() {
+    const {
+      seek,
+      seekingHandler,
+      progress,
+      component: Component,
+      ...otherProps
+    } = this.props;
+
+    return (
+      <Component
+        progress={progress}
+        onSeek={seek}
+        onSeeking={seekingHandler}
+        {...otherProps}
+      />
+    );
+  }
+}
+
+const enhance = compose(
+  getContext({
+    seek: PropTypes.func,
+  }),
+);
+
+export default enhance(AudioSeeker);
