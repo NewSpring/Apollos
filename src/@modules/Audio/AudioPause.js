60a514fbcf3617e4f5c63b4c289d677f1fa3111a
Marcelo Reyna <Lepozepo@gmail.com>
2017-10-25 13:48:30 -0700

    Build play and pause helpers

---
+++ src/@modules/Audio/AudioPause.js
@@ -0,0 +1,37 @@
+import React, { PureComponent } from 'react';
+import getContext from 'recompose/getContext';
+import compose from 'recompose/compose';
+import PropTypes from 'prop-types';
+import {
+  TouchableHighlight,
+} from 'react-native';
+import onlyPropTypes from '../../@utils/onlyPropTypes';
+
+export class AudioPause extends PureComponent {
+  static propTypes = {
+    pause: PropTypes.func.isRequired,
+  };
+
+  render() {
+    const {
+      pause,
+      ...otherProps
+    } = this.props;
+
+    return (
+      <TouchableHighlight
+        onPress={pause}
+        {...otherProps}
+      />
+    );
+  }
+}
+
+const enhance = compose(
+  getContext({
+    pause: PropTypes.func,
+  }),
+  onlyPropTypes,
+);
+
+export default enhance(AudioPause);
