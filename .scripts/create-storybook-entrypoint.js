1044ae5bd638ad418d47157049f70d58cbe3a29e
Marcelo Reyna <Lepozepo@gmail.com>
2017-10-31 11:11:50 -0700

    Patch storybook web (#46)
    * Fix webpack config for storybook web
    
    * Change command to storybook-web
    
    * Patch Audio story
    
    * removing unneeded RN build command from storybook-web.
    
    * appeasing linter
    
    * updated and cleaned up gitignore to removed system files.
    
    * adding yarn-error to gitignore

--- .scripts/create-storybook-entrypoint.js
+++ .scripts/create-storybook-entrypoint.js
@@ -11,5 +11,5 @@ export default StorybookUI;
 try {
   FS.writeFileSync(Path.resolve(__dirname, '../App.js'), content);
 } catch (err) {
-  console.error(err);
+  console.error(err); // eslint-disable-line no-console
 }
--- src/@modules/Audio/Audio.stories.js
+++ src/@modules/Audio/Audio.stories.js
@@ -1,6 +1,11 @@
 import React from 'react';
 
 import { storiesOf } from '@storybook/react-native';
+import {
+  View,
+} from 'react-native';
+import Play from '../../@primitives/icons/Play';
+import Pause from '../../@primitives/icons/Pause';
 
 import ThemeProvider from '../../@primitives/ThemeProvider';
 import Audio from './';
@@ -8,7 +13,21 @@ import Audio from './';
 storiesOf('Audio', module)
   .add('renders', () => (
     <ThemeProvider>
-      <Audio source="https://www.w3schools.com/html/horse.mp3" />
+      <Audio source="https://www.w3schools.com/html/horse.mp3">
+        <Audio.Play>
+          <View>
+            <Play />
+          </View>
+        </Audio.Play>
+
+        <Audio.Pause>
+          <View>
+            <Pause />
+          </View>
+        </Audio.Pause>
+
+        <Audio.Seeker />
+      </Audio>
     </ThemeProvider>
   ));
 
