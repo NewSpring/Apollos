46f4a900c814cd6bf846cebf4ae999e4a9517cf2
Conrad VanLandingham <hi@convan.me>
2017-11-09 10:00:05 -0600

    fix the way `withTheme` maps so we don't leak the entire theme def into components

--- src/@primitives/withTheme.js
+++ src/@primitives/withTheme.js
@@ -1,17 +1,20 @@
 import PropTypes from 'prop-types';
 import getContext from 'recompose/getContext';
 import compose from 'recompose/compose';
-import withProps from 'recompose/withProps';
+import mapProps from 'recompose/mapProps';
 import { THEME_PROPS } from './constants';
 
 
-const DEFAULT_MAPPER_FN = ({ theme, ...otherProps } = {}) => ({ ...theme, ...otherProps });
+const DEFAULT_MAPPER_FN = ({ theme } = {}) => ({ ...theme });
 
 export default function (mapperFn = DEFAULT_MAPPER_FN) {
   return compose(
     getContext({
       theme: PropTypes.shape(THEME_PROPS),
     }),
-    withProps(mapperFn),
+    mapProps(({ theme, otherProps }) => ({
+      ...otherProps,
+      ...mapperFn({ theme, ...otherProps }),
+    })),
   );
 }
