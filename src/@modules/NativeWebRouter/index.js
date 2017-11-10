e3adb5b00de943c54c72462734e2b63c6128ad96
Conrad VanLandingham <hi@convan.me>
2017-11-10 12:05:26 -0600

    add in active states for tab bar

--- src/@modules/NativeWebRouter/index.js
+++ src/@modules/NativeWebRouter/index.js
@@ -1,5 +1,6 @@
 export { default as Router } from './Router';
 export { default as Link } from './Link';
-export { default as NavLink } from './NavLink';
 export { default as AndroidBackButton } from './AndroidBackButton';
+export { default as matchLocationToPath } from './matchLocationToPath';
+
 export { Route, Switch, withRouter, Redirect, matchPath } from 'react-router';
