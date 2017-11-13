ad82f8163fa488d7c1d7dd9e06c8cdecd98cd2be
Conrad VanLandingham <hi@convan.me>
2017-11-13 01:37:29 -0600

    add in touch / pan ability

--- src/@modules/CardStack/Transitioner.js
+++ src/@modules/CardStack/Transitioner.js
@@ -1,6 +1,7 @@
-import React, { PureComponent, cloneElement } from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
-import { Animated, StyleSheet, View, Easing } from 'react-native';
+import { Animated, StyleSheet, View, Easing, PanResponder } from 'react-native';
+import { clamp } from 'lodash';
 import interpolator from './interpolator';
 import findFirstMatch from './findFirstMatch';
 
@@ -17,6 +18,11 @@ class Transitioner extends PureComponent {
     children: PropTypes.node,
     history: PropTypes.shape({
       action: PropTypes.string,
+      index: PropTypes.number,
+      canGo: PropTypes.func,
+      goBack: PropTypes.func,
+      goForward: PropTypes.func,
+      push: PropTypes.func,
     }),
     location: PropTypes.shape({
       key: PropTypes.string,
@@ -24,6 +30,7 @@ class Transitioner extends PureComponent {
     match: PropTypes.shape({
       isExact: PropTypes.bool,
     }),
+    width: PropTypes.number.isRequired,
   };
 
   static defaultProps = {
@@ -57,6 +64,18 @@ class Transitioner extends PureComponent {
         previousLocation: this.props.location,
         transition,
       }, () => {
+        if (this.isPanning) return;
+
+        // Don't animate on nested routes. We can check for nested routes by comparing
+        // computedMatch.path - both the current and last route will be pointing to the
+        // same "route" === same match)
+        if (this.lastAndCurrentRoute.current.props.computedMatch.path
+          === this.lastAndCurrentRoute.last.props.computedMatch.path
+        ) {
+          this.setState({ transition: null });
+          return;
+        }
+
         this.animatedPosition.setValue(fromPosition);
         this.animation = Animated.timing(this.animatedPosition, {
           duration: ANIMATION_DURATION,
@@ -93,7 +112,101 @@ class Transitioner extends PureComponent {
     return { current, last };
   }
 
+  startingIndex = this.props.history.index;
+
   animatedPosition = new Animated.Value(0);
+
+  panResponder = PanResponder.create({
+    onMoveShouldSetPanResponder: (event, gesture) => (
+      this.props.history.index > this.startingIndex &&
+      this.props.history.canGo(-1) &&
+      event.nativeEvent.pageX < GESTURE_RESPONSE_DISTANCE_HORIZONTAL &&
+      gesture.dx > RESPOND_THRESHOLD
+    ),
+
+    onPanResponderGrant: () => {
+      this.animatedPosition.stopAnimation(() => {
+        this.isPanning = true;
+        // this.gestureStartValue = value;
+        this.props.history.goBack();
+      });
+    },
+
+    onPanResponderMove: (event, { dx }) => {
+      // Handle the moving touches for our granted responder
+      const startValue = 1; // todo: should we be utilizing gestureStartValue (above) somehow?
+      const currentValue = startValue + (-dx / this.props.width);
+      const value = clamp(0, currentValue, 1);
+      this.animatedPosition.setValue(value);
+    },
+
+    onPanResponderRelease: (event, { dx, vx }) => {
+      // Calculate animate duration according to gesture speed and moved distance
+      const movedDistance = dx;
+      const gestureVelocity = vx;
+      const defaultVelocity = this.props.width / ANIMATION_DURATION;
+      const velocity = Math.max(Math.abs(gestureVelocity), defaultVelocity);
+      const resetDuration = movedDistance / velocity;
+      const goBackDuration = (this.props.width - movedDistance) / velocity;
+
+      // To asyncronously get the current animated value, we need to run stopAnimation:
+      this.animatedPosition.stopAnimation((value) => {
+        // If the speed of the gesture release is significant, use that as the indication of intent
+        if (gestureVelocity < -0.5) {
+          this.cancelNavigationFromPan(resetDuration);
+          return;
+        }
+        if (gestureVelocity > 0.5) {
+          this.finishNavigationFromPan(goBackDuration);
+          return;
+        }
+
+        // Then filter based on the distance the screen was moved. Over a third of the way swiped,
+        // and the back will happen.
+        if (value <= POSITION_THRESHOLD) {
+          this.finishNavigationFromPan(goBackDuration);
+        } else {
+          this.cancelNavigationFromPan(resetDuration);
+        }
+      });
+    },
+  });
+
+  cancelNavigationFromPan = (duration) => {
+    this.animation = Animated.timing(this.animatedPosition, {
+      toValue: 1,
+      duration,
+      easing: Easing.linear(),
+      useNativeDriver: true,
+    }).start(({ finished }) => {
+      if (finished) {
+        this.props.history.goForward();
+        this.afterPan();
+      }
+    });
+  };
+
+  finishNavigationFromPan = (duration) => {
+    Animated.timing(this.animatedPosition, {
+      toValue: 0,
+      duration,
+      easing: Easing.linear(),
+      useNativeDriver: true,
+    }).start(({ finished }) => {
+      if (finished) {
+        this.afterPan();
+      }
+    });
+  };
+
+  afterPan = () => {
+    this.isPanning = false;
+
+    this.setState({
+      previousLocation: {},
+      transition: null,
+    });
+  };
 
   renderScreens() {
     let screens = [];
@@ -110,7 +223,7 @@ class Transitioner extends PureComponent {
           key: this.props.location.key,
         }),
       ];
-    } else if (this.state.transition === POP) {
+    } else if (this.state.transition === POP || this.isPanning) {
       screens = [
         this.renderScreenWithAnimation({
           index: 0,
@@ -125,11 +238,9 @@ class Transitioner extends PureComponent {
       ];
     } else {
       screens = [
-        this.renderScreenWithAnimation({
-          index: 0,
-          screen: this.lastAndCurrentRoute.current,
-          key: this.props.location.key,
-        }),
+        <Animated.View key={this.props.location.key} style={[StyleSheet.absoluteFill, { backgroundColor: 'white' }]}>
+          {this.lastAndCurrentRoute.current}
+        </Animated.View>,
       ];
     }
     return screens;
@@ -155,7 +266,7 @@ class Transitioner extends PureComponent {
 
   render() {
     return (
-      <View style={[StyleSheet.absoluteFill, { backgroundColor: '#000' }]}>
+      <View style={[StyleSheet.absoluteFill, { backgroundColor: '#000' }]} {...this.panResponder.panHandlers}>
         {this.renderScreens()}
       </View>
     );
