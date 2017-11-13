9f5309c84dbf5e7a2ead3936ec0217fe8fe42702
Marcelo Reyna <Lepozepo@gmail.com>
2017-11-13 16:29:09 -0600

    Patch fetchMoreResolver

--- src/@data/utils/fetchMoreResolver.js
+++ src/@data/utils/fetchMoreResolver.js
@@ -4,11 +4,11 @@ export default function fetchMoreResolver({ collectionName, mapTo, data } = {}) 
     data.fetchMore({
       variables: { ...data.variables, skip: data[collectionName].length },
       updateQuery: (previousResult, { fetchMoreResult }) => {
-        if (!fetchMoreResult.data) return previousResult;
+        if (!fetchMoreResult) return previousResult;
         return {
           [mapTo || collectionName]: [
             ...previousResult[collectionName],
-            ...fetchMoreResult.data[collectionName],
+            ...fetchMoreResult[collectionName],
           ],
         };
       },
