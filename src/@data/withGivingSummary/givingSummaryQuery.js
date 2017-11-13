b93928d0d345d0016c1d8958aa47d56853cb9cc1
Marcelo Reyna <Lepozepo@gmail.com>
2017-11-13 14:18:32 -0600

    withGivingSummary

--- src/@data/withGivingSummary/givingSummaryQuery.js
+++ src/@data/withGivingSummary/givingSummaryQuery.js
@@ -2,7 +2,7 @@ import { gql } from 'react-apollo';
 
 export default gql`
   query givingSummary($start: String!, $end: String!) {
-    transactions(start: $start, end: $end, limit: 0) {
+    content: transactions(start: $start, end: $end, limit: 0) {
       id
       date
       details {
