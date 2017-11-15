8a479efe77c63390f58060c431af4f5d98378e07
Burke Shartsis <burke@burkeshartsis.com>
2017-11-15 10:45:54 -0500

    fixed test to include theme props

--- src/@modules/Header/Header.tests.js
+++ src/@modules/Header/Header.tests.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import ThemeProvider from '@primitives/ThemeProvider';
+
 import Header from './';
 
 describe('The Header component', () => {
   it('should render correctly', () => {
     const tree = renderer.create(
-      <Header />,
+      <ThemeProvider>
+        <Header />
+      </ThemeProvider>,
     );
     expect(tree).toMatchSnapshot();
   });
