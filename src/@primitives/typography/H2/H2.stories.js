a6cc61a4b9aefc167558f0e6e27c99693862b21e
Burke Shartsis <burke@burkeshartsis.com>
2017-11-14 22:35:48 -0500

    reshuffling typography stories

--- src/@primitives/typography/H2/H2.stories.js
+++ src/@primitives/typography/H2/H2.stories.js
@@ -5,8 +5,8 @@ import { storiesOf } from '@storybook/react-native';
 import ThemeProvider from '@primitives/ThemeProvider';
 import H2 from './';
 
-storiesOf('Typography/Headings/H1', module)
-  .add('default', () => (
+storiesOf('Typography/Headings', module)
+  .add('H2', () => (
     <ThemeProvider>
       <H2>{'"God is most glorified in us when we are most satisfied in Him" ― John Piper'}</H2>
     </ThemeProvider>
