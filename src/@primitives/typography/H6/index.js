e892f18b37a220121cdda8532e46e08c36afbd06
Burke Shartsis <burke@burkeshartsis.com>
2017-11-16 14:27:51 -0500

    updated H6 component styles and structure.

--- src/@primitives/typography/H6/index.js
+++ src/@primitives/typography/H6/index.js
@@ -2,35 +2,35 @@ import React from 'react';
 import { Text } from 'react-native';
 import { compose, pure, setPropTypes } from 'recompose';
 import PropTypes from 'prop-types';
-import withTheme from '@primitives/withTheme';
+import styled from '@primitives/styled';
+import rem from '@utils/remUnit';
+import verticalRhythm from '@utils/verticalRhythm';
 
 const enhance = compose(
   pure,
-  withTheme(({ theme: { baseFontColor } }) => ({ baseFontColor })),
   setPropTypes({
     children: PropTypes.node,
     style: Text.propTypes.style,
   }),
 );
 
+const StyledH6 = styled(({ theme }) => ({
+  color: theme.baseFontColor,
+  fontSize: rem(0.875),
+  lineHeight: verticalRhythm(0.875, 1.02),
+}))(Text);
+
 const H6 = enhance(({
   children,
   style: styleProp = {},
-  baseFontColor,
   ...otherProps
-}) => {
-  const tempStyle = { // TODO: replace this with passing theme props into our styled HOC
-    color: baseFontColor,
-  };
-
-  return (
-    <Text
-      style={[tempStyle, styleProp]}
-      {...otherProps}
-    >
-      {children}
-    </Text>
-  );
-});
+}) => (
+  <StyledH6
+    style={styleProp}
+    {...otherProps}
+  >
+    {children}
+  </StyledH6>
+));
 
 export default H6;
