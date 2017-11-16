a23edaf438b64b0e4df5c23d6c2170342aca5dfb
Burke Shartsis <burke@burkeshartsis.com>
2017-11-16 13:51:34 -0500

    updated H2 component styles and structure

--- src/@primitives/typography/H2/index.js
+++ src/@primitives/typography/H2/index.js
@@ -2,35 +2,36 @@ import React from 'react';
 import { Text } from 'react-native';
 import { compose, pure, setPropTypes } from 'recompose';
 import PropTypes from 'prop-types';
-import withTheme from '@primitives/withTheme';
+import styled from '@primitives/styled';
+import rem from '@utils/remUnit';
+import verticalRhythm from '@utils/verticalRhythm';
 
 const enhance = compose(
   pure,
-  withTheme(({ theme: { baseFontColor } }) => ({ baseFontColor })),
   setPropTypes({
     children: PropTypes.node,
     style: Text.propTypes.style,
   }),
 );
 
+const StyledH2 = styled(({ theme }) => ({
+  color: theme.baseFontColor,
+  fontSize: rem(2.35),
+  lineHeight: verticalRhythm(2.35, 1.145),
+}))(Text);
+
 const H2 = enhance(({
   children,
   style: styleProp = {},
   baseFontColor,
   ...otherProps
-}) => {
-  const tempStyle = { // TODO: replace this with passing theme props into our styled HOC
-    color: baseFontColor,
-  };
-
-  return (
-    <Text
-      style={[tempStyle, styleProp]}
-      {...otherProps}
-    >
-      {children}
-    </Text>
-  );
-});
+}) => (
+  <StyledH2
+    style={styleProp}
+    {...otherProps}
+  >
+    {children}
+  </StyledH2>
+));
 
 export default H2;
