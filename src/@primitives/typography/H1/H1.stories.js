47ff55ddaa179bd77c3ee41cf3af5796acbdee53
Burke Shartsis <burke@burkeshartsis.com>
2017-11-16 13:39:28 -0500

    updated H1 component styles and structure

--- src/@primitives/typography/H1/H1.stories.js
+++ src/@primitives/typography/H1/H1.stories.js
@@ -8,6 +8,6 @@ import H1 from './';
 storiesOf('@primitives/typography', module)
   .add('H1', () => (
     <ThemeProvider>
-      <H1>{'"Do all the good you can. By all the means you can. In all the ways you can. In all theplaces you can. At all the times you can. To all the people you can. As long as ever you can." ― John Wesley'}</H1>
+      <H1>{'"Do all the good you can. By all the means you can. In all the ways you can. In all the places you can. At all the times you can. To all the people you can. As long as ever you can." ― John Wesley'}</H1>
     </ThemeProvider>
   ));
--- src/@primitives/typography/H1/index.js
+++ src/@primitives/typography/H1/index.js
@@ -2,35 +2,38 @@ import React from 'react';
 import { Text } from 'react-native';
 import { compose, pure, setPropTypes } from 'recompose';
 import PropTypes from 'prop-types';
-import withTheme from '@primitives/withTheme';
+// import withTheme from '@primitives/withTheme';
+import styled from '@primitives/styled';
+import rem from '@utils/remUnit';
+import verticalRhythm from '@utils/verticalRhythm';
 
 const enhance = compose(
   pure,
-  withTheme(({ theme: { primaryColor } }) => ({ primaryColor })),
+  // withTheme(({ theme: { primaryColor } }) => ({ primaryColor })),
   setPropTypes({
     children: PropTypes.node,
     style: Text.propTypes.style,
   }),
 );
 
+const StyledH1 = styled(({ theme }) => ({
+  color: theme.primaryColor,
+  fontSize: rem(2.9),
+  lineHeight: verticalRhythm(2.9, 0.945),
+}))(Text);
+
 const H1 = enhance(({
   children,
   style: styleProp = {},
   primaryColor,
   ...otherProps
-}) => {
-  const dynamicStyle = { // TODO: replace this with passing theme props into our styled HOC
-    color: primaryColor,
-  };
-
-  return (
-    <Text
-      style={[dynamicStyle, styleProp]}
-      {...otherProps}
-    >
-      {children}
-    </Text>
-  );
-});
+}) => (
+  <StyledH1
+    style={styleProp}
+    {...otherProps}
+  >
+    {children}
+  </StyledH1>
+));
 
 export default H1;
