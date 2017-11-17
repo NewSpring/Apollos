64a38ca8d910fd90d74f326708be0ac675ec8efa
Conrad VanLandingham <hi@convan.me>
2017-11-17 09:56:50 -0600

    fix linter error

--- src/@primitives/styled/__tests__/createStyleSheet.tests.js
+++ src/@primitives/styled/__tests__/createStyleSheet.tests.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { StyleSheet, View } from 'react-native';
+import { View } from 'react-native';
 import renderer from 'react-test-renderer';
 import createStyleSheet, { cachedStyles } from '../createStyleSheet';
 
