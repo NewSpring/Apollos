21ab2d174ac253cf47c61e4602fb4b78aa251e6a
Conrad VanLandingham <conrad@landdolph.in>
2017-11-21 17:34:07 -0500

    Make responsive modals work with screen resizes

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -8,6 +8,8 @@ import CardStack from '@modules/CardStack';
 import { asModal } from '@primitives/ModalView';
 import * as tabs from './tabs';
 
+let previousLocation;
+
 class AppRouter extends PureComponent {
   static propTypes = {
     location: PropTypes.shape({
@@ -17,23 +19,26 @@ class AppRouter extends PureComponent {
     isLargeScreen: PropTypes.bool,
   }
 
+  componentWillMount() {
+    if (!previousLocation) previousLocation = this.props.location;
+  }
+
   componentWillUpdate(nextProps) {
     if (nextProps.history.action !== 'POP' &&
-        (!this.props.location.state || !this.isModal)) {
-      this.previousLocation = this.props.location;
+        (!this.props.location.state || !this.isModal)
+    ) {
+      previousLocation = this.props.location;
     }
   }
 
   get isModal() {
     return this.props.isLargeScreen &&
-      this.previousLocation &&
-      this.previousLocation.pathname !== this.props.location.pathname &&
+      previousLocation &&
+      previousLocation.pathname !== this.props.location.pathname &&
       this.largeScreenModals.find(route =>
         matchPath(this.props.location.pathname, route.props.path),
       );
   }
-
-  previousLocation = this.props.location;
 
   // On large screens we render modals on top of the previous route.
   // These routes should also exist elsewhere in the routing stack, which
@@ -69,7 +74,7 @@ class AppRouter extends PureComponent {
     return (
       <View style={{ flex: 1 }}>
         {Platform.OS === 'android' ? <AndroidBackButton /> : null}
-        <AppSwitch location={this.isModal ? this.previousLocation : this.props.location}>
+        <AppSwitch location={this.isModal ? previousLocation : this.props.location}>
           <Route component={this.tabs} />
         </AppSwitch>
         {this.isModal ? this.largeScreenModals : null}
