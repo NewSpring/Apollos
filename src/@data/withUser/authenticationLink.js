c18e72ddacda52032d8c0dca006cd45685213be3
Marcelo Reyna <Lepozepo@gmail.com>
2017-11-27 11:55:29 -0800

    Continue sending messages on critical failure

--- src/@data/withUser/authenticationLink.js
+++ src/@data/withUser/authenticationLink.js
@@ -5,17 +5,23 @@ import { setContext } from 'apollo-link-context';
 import isEmpty from 'lodash/isEmpty';
 
 export default setContext(async (_, { headers }) => {
-  // get the authentication token from local storage if it exists
-  const token = await AsyncStorage.getItem('authToken');
-  if (!token) return {};
-  const [email, password] = token.split('::');
-  if (isEmpty(email) || isEmpty(password)) return {};
+  try {
+    // get the authentication token from local storage if it exists
+    const token = await AsyncStorage.getItem('authToken');
+    if (!token) return {};
+    const [email, password] = token.split('::');
+    if (isEmpty(email) || isEmpty(password)) return {};
 
-  // return the headers to the context so httpLink can read them
-  return {
-    headers: {
-      ...headers,
-      'Authorization-Token': `${encodeURIComponent(atob(email))}:${encodeURIComponent(atob(password))}`,
-    },
-  };
+    // return the headers to the context so httpLink can read them
+    return {
+      headers: {
+        ...headers,
+        'Authorization-Token': `${encodeURIComponent(atob(email))}:${encodeURIComponent(atob(password))}`,
+      },
+    };
+  } catch (err) {
+    // eslint-disable-next-line
+    console.error('Authorization Failed', err);
+    return {};
+  }
 });
