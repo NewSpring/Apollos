a933381165f6996dca079c80f2bcd5859fb15615
Marcelo Reyna <Lepozepo@gmail.com>
2017-11-27 10:37:59 -0800

    Use LoginForm under Profile

--- src/tabs/Profile.js
+++ src/tabs/Profile.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '@modules/Header';
 import FlexedView from '@primitives/FlexedView';
+import LoginForm from '@modules/LoginForm';
 
 export default function Profile() {
   return (
     <FlexedView>
       <Header titleText="Profile" />
+      <LoginForm />
     </FlexedView>
   );
 }
