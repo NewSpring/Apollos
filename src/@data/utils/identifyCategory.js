382ba87b2ceb737db24cb2746ea24a1b7c9865d5
Marcelo Reyna <Lepozepo@gmail.com>
2017-11-28 13:17:19 -0800

    Simplify default category case

--- src/@data/utils/identifyCategory.js
+++ src/@data/utils/identifyCategory.js
@@ -28,18 +28,9 @@ export default function identifyCategory(item) {
         category: 'need to know', // NOTE: this is very odd
       };
     default: {
-      let contentCategory = category;
-      // XXX this handles the case of `articless` but not `articles`
-      // i think this is wrong
-      if (category.charAt(category.length - 2) === 's') {
-        // capitlize first letter and remove "s" from end
-        contentCategory += category.substr(1, category.length - 2);
-      } else {
-        contentCategory += category.substr(1, category.length - 1);
-      }
       return {
         ...item,
-        category: contentCategory,
+        category: category.replace(/(.)(?=.*\1)/g, ''),
       };
     }
   }
