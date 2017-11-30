0dca77526547829fa9cb75cefca33225f616ae5f
Conrad VanLandingham <hi@convan.me>
2017-11-30 08:27:50 -0600

    tip adding htmlview and contentview components

--- src/articles/Single.js
+++ src/articles/Single.js
@@ -1,11 +1,34 @@
+import React from 'react';
+import { ScrollView, Image } from 'react-native';
 import { compose, mapProps, pure } from 'recompose';
-import DebugView from '@primitives/DebugView';
+import { H1, H7 } from '@primitives/typography';
+import FlexedView from '@primitives/FlexedView';
+import ContentView from '@primitives/ContentView';
 import withArticle from '@data/withArticle';
 
-const ArticleSingle = compose(
+const enhance = compose(
   pure,
   mapProps(({ match: { params: { id } } }) => ({ id })),
   withArticle,
-)(DebugView);
+);
+
+const image = ({ images = [] } = {}) => images && images[0] && `https:${images[0].url}`;
+
+const ArticleSingle = enhance(({
+  content: {
+    authors = [],
+    title = '',
+    content = {},
+  } = { },
+}) => console.log({ content, image: image(content) }) || (
+  <FlexedView>
+    <ScrollView>
+      {image(content) ? <Image style={{ width: '100%', height: 200 }} source={{ uri: image(content) }} /> : null}
+      <H1>{title}</H1>
+      {authors.length ? <H7>By: {authors.join(', ')}</H7> : null}
+      <ContentView body={content.body} />
+    </ScrollView>
+  </FlexedView>
+));
 
 export default ArticleSingle;
