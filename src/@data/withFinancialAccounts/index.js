2825446a3679a61838830fe3906f70cf8fd1a16e
Marcelo Reyna <Lepozepo@gmail.com>
2017-11-30 12:16:36 -0800

    Add isLoading to financial accounts query

--- src/@data/withFinancialAccounts/index.js
+++ src/@data/withFinancialAccounts/index.js
@@ -2,8 +2,9 @@ import { graphql } from 'react-apollo';
 import financialAccountsQuery from './financialAccountsQuery';
 
 export default graphql(financialAccountsQuery, {
-  props: ({ data: { accounts } }) => ({
-    accounts,
+  props: ({ data }) => ({
+    accounts: data.accounts,
+    isLoading: data.loading,
   }),
 });
 
