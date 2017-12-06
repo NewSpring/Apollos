bba44774086ee5962b506bf2d68d1a06f06ff59c
Conrad VanLandingham <hi@convan.me>
2017-12-06 12:41:54 -0600

    fix more linter errors and snapshots

--- src/@primitives/FeedItemCard/CardImage/index.js
+++ src/@primitives/FeedItemCard/CardImage/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { StyleSheet, View, Platform, Dimensions } from 'react-native';
+import { StyleSheet, View, Platform } from 'react-native';
 import PropTypes from 'prop-types';
-import { compose, pure, setPropTypes, withState } from 'recompose';
+import { compose, pure, setPropTypes } from 'recompose';
 import Color from 'color';
 
 import withTheme from '@primitives/withTheme';
