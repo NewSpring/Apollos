2ff22a80706b531f58ec1f015a5ec226ff3edc5b
Marcelo Reyna <Lepozepo@gmail.com>
2017-12-05 16:04:11 -0800

    Start bringing in create order

---
+++ src/@data/withCheckout/formatPersonDetails.js
@@ -0,0 +1,104 @@
+import moment from 'moment';
+
+// TODO: Refactor / review
+export default function formatPersonDetails(give) {
+  const {
+    data,
+    transactions,
+    total,
+    schedule,
+    savedAccount,
+  } = give;
+
+  // here we format data for the NMI processing
+  const joinedData = {
+    amount: total,
+    billing: {
+      'first-name': data.personal.firstName,
+      'last-name': data.personal.lastName,
+      email: data.personal.email,
+      address1: data.billing.streetAddress,
+      address2: data.billing.streetAddress2 || '',
+      city: data.billing.city,
+      state: data.billing.state,
+      postal: data.billing.zip,
+    },
+  };
+
+  if (give.transactionType === 'savedPayment') delete joinedData.amount;
+
+  const { campusId } = data.personal;
+  joinedData['merchant-defined-field-2'] = campusId;
+
+  if (schedule && schedule.start) {
+    // // @TODO allow custom start dates
+    // joinedData['start-date'] = moment().format('YYYYMMDD')
+    // @TODO allow number of payments
+    joinedData.plan = {
+      payments: 0,
+      amount: total,
+    };
+
+    delete joinedData.amount;
+    switch (schedule.frequency) {
+      case 'One-Time':
+        joinedData.plan.payments = 1;
+        joinedData.plan['month-frequency'] = 12;
+        joinedData.plan['day-of-month'] = schedule.start
+          ? moment(schedule.start).date()
+          : moment().date();
+        break;
+      case 'Weekly':
+        joinedData.plan['day-frequency'] = 7;
+        break;
+      case 'Bi-Weekly':
+        joinedData.plan['day-frequency'] = 14;
+        break;
+      case 'Monthly':
+        joinedData.plan['month-frequency'] = 1;
+        joinedData.plan['day-of-month'] = schedule.start
+          ? moment(schedule.start).date()
+          : moment().date();
+        break;
+      default:
+        break;
+    }
+
+    joinedData['start-date'] = schedule.start
+      ? moment(schedule.start).format('YYYYMMDD')
+      : moment().add(1, 'days').format('YYYYMMDD');
+    joinedData['merchant-defined-field-3'] = joinedData['start-date'];
+
+    // This isn't super well organized
+    // in the beginning we didn't support multiple accounts
+    // on a schedule
+    // now we do but we have to support existing builds so
+    // we create a comma sep string and split on heighliner
+    joinedData['merchant-defined-field-1'] = Object.keys(transactions).join(',');
+
+    // in order to line up the amounts with the funds, we store the amounts
+    // in a matching comma sep string
+    joinedData['merchant-defined-field-4'] = Object.keys(transactions)
+      .map(key => transactions[key].value)
+      .join(',');
+  } else if (transactions && Object.keys(transactions).length) {
+    joinedData.product = [];
+    // eslint-disable-next-line no-restricted-syntax, guard-for-in
+    for (const transaction in transactions) {
+      joinedData.product.push({
+        quantity: 1,
+        'product-code': transaction,
+        description: transactions[transaction].label,
+        'total-amount': transactions[transaction].value,
+        'unit-cost': transactions[transaction].value,
+      });
+    }
+  }
+
+  if (savedAccount.id) {
+    joinedData.savedAccount = savedAccount.id;
+    joinedData.savedAccountName = savedAccount.name;
+  }
+
+  return joinedData;
+}
--- src/@data/withCheckout/index.js
+++ src/@data/withCheckout/index.js
@@ -1,7 +1,12 @@
 import { graphql } from 'react-apollo';
+import { compose } from 'recompose';
 import checkoutQuery from './checkoutQuery';
+import createOrderMutation from './createOrderMutation';
 
-export default graphql(checkoutQuery, {
+// May need to remap props here
+import formatPersonDetails from './formatPersonDetails';
+
+export const withCheckoutQuery = graphql(checkoutQuery, {
   options: { variables: { state: 28, country: 45 } },
   props({ data }) {
     const {
@@ -24,3 +29,20 @@ export default graphql(checkoutQuery, {
   },
 });
 
+// NOTE: They create order after capturing a billing address
+export const withCheckoutMutation = graphql(createOrderMutation, {
+  props: ({ mutate }) => ({
+    createOrder: data => (mutate({
+      variables: {
+        data: JSON.stringify(formatPersonDetails(data)),
+        id: null,
+        instant: false,
+      },
+    })),
+  }),
+});
+
+export default compose(
+  withCheckoutMutation,
+  withCheckoutQuery,
+);
