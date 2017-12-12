7c186a91f0411da1b2f2a9f5c5fe48eeb50041fd
Burke Shartsis <burke@burkeshartsis.com>
2017-12-12 10:05:44 -0500

    replace shadow decleration on card wrapper with default theme shadow (same).

--- src/@ui/CardWrapper/index.js
+++ src/@ui/CardWrapper/index.js
@@ -17,23 +17,7 @@ const StyledCard = styled(({ theme, cardColor }) => ({
   width: '100%',
   backgroundColor: cardColor || theme.colors.lightPrimary,
   borderRadius: theme.sizing.borderRadius,
-  ...Platform.select({
-    ios: {
-      shadowColor: theme.colors.lightTertiary,
-      shadowOffset: {
-        width: 0,
-        height: 1,
-      },
-      shadowOpacity: 1,
-      shadowRadius: 3,
-    },
-    android: {
-      elevation: 3,
-    },
-    web: {
-      boxShadow: `0 1px 4px ${theme.colors.lightTertiary}`,
-    },
-  }),
+  ...Platform.select(theme.shadows.default),
 }))(View);
 
 /*
