e57f0619f7916beb6f72a2df3f6f52be2989c3dd
Conrad VanLandingham <hi@convan.me>
2017-12-14 15:21:18 -0600

    add node modules to compile to prod webpack

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -12,6 +12,7 @@ const eslintFormatter = require('react-dev-utils/eslintFormatter');
 const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin');
 const paths = require('./paths');
 const getClientEnvironment = require('./env');
+const nodeModulesToCompile = require('./nodeModulesToCompile');
 
 // Webpack uses `publicPath` to determine where the app is being served from.
 // It requires a trailing slash, or the file assets will get an incorrect path.
