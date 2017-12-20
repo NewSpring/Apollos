a31c0d2e7342d296db5613e73c4b8336800065d9
Marcelo Reyna <Lepozepo@gmail.com>
2017-12-20 11:51:37 -0800

    Make it easy to swap out dev code for now

--- src/@data/Client/httpLink.js
+++ src/@data/Client/httpLink.js
@@ -1,3 +1,4 @@
 import { createHttpLink } from 'apollo-link-http';
 
 export default createHttpLink({ uri: 'https://api.newspring.cc/graphql' });
+// export default createHttpLink({ uri: 'http://localhost:8888/graphql' });
