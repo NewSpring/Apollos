98ec30fd34138d0593a01cc467fa3b20ab0ea204
Burke Shartsis <burke@burkeshartsis.com>
2017-12-21 12:31:00 -0500

    WIP refactored to include loading state skeleton. needs proper colors and to be checked on web. Otherwise it's mostly there.

--- src/@ui/CardTile/index.js
+++ src/@ui/CardTile/index.js
@@ -3,25 +3,37 @@ import { Platform, View } from 'react-native';
 import PropTypes from 'prop-types';
 import { compose, pure, setPropTypes, defaultProps } from 'recompose';
 
-
+import { withTheme } from '@ui/theme';
 import styled from '@ui/styled';
-import { H6, UIText } from '@ui/typography';
+import { H4, H6, H7, UIText } from '@ui/typography';
+import Placeholder from 'rn-placeholder';
+import Icon from '@ui/Icon';
 
 const enhance = compose(
   pure,
+  withTheme(),
   setPropTypes({
     number: PropTypes.number.isRequired,
+    title: PropTypes.string,
+    byLine: PropTypes.string,
+    date: PropTypes.string,
     style: PropTypes.any, // eslint-disable-line
+    isLoading: PropTypes.bool,
   }),
   defaultProps({
     number: 1,
+    title: 'Sermon Title',
+    byLine: 'Marty McFly',
+    date: '3mo',
+    isLoading: false,
   }),
 );
 
 const Tile = styled(({ theme }) => ({
-  justifyContent: 'center',
   width: '80%',
   aspectRatio: 1,
+  justifyContent: 'center',
+  paddingHorizontal: theme.sizing.baseUnit,
   borderRadius: theme.sizing.borderRadius,
   overflow: 'hidden',
   backgroundColor: theme.colors.lightTertiary, // TODO: sort out correct theme var for this color
@@ -51,9 +63,25 @@ const TileNumber = styled(({ theme, size }) => ({
   backgroundColor: theme.colors.background.default,
 }))(View);
 
+const ByLineWrapper = styled(({ theme }) => ({
+  flexDirection: 'row',
+  alignItems: 'center',
+  paddingTop: theme.sizing.baseUnit / 2,
+}))(View);
+
+const PlaceholderWrapper = styled(({ theme }) => ({
+  flex: 1,
+  paddingHorizontal: theme.sizing.baseUnit / 2,
+}))(View);
+
 const CardTile = enhance(({
   number,
+  title,
+  byLine,
+  date,
   style: styleProp = {},
+  isLoading,
+  theme,
   ...otherProps
 }) => (
   <Tile style={styleProp} {...otherProps}>
@@ -62,7 +90,39 @@ const CardTile = enhance(({
         <H6>{number}</H6>
       </View>
     </TileNumber>
-    <UIText>Boom</UIText>
+
+    <Placeholder.Line
+      width={'75%'}
+      textSize={theme.helpers.rem(1.4)}
+      onReady={!isLoading}
+    >
+      <H4>{title}</H4>
+    </Placeholder.Line>
+
+    <ByLineWrapper>
+      <Icon
+        name={'video'}
+        size={theme.helpers.rem(1)}
+        fill={theme.colors.text.primary}
+        isLoading={isLoading}
+      />
+      <PlaceholderWrapper>
+        <Placeholder.Line
+          width={'40%'}
+          textSize={theme.helpers.rem(1)}
+          onReady={!isLoading}
+        >
+          <H7>{byLine}</H7>
+        </Placeholder.Line>
+      </PlaceholderWrapper>
+      <Placeholder.Line
+        width={'10%'}
+        textSize={theme.helpers.rem(1)}
+        onReady={!isLoading}
+      >
+        <H7>{date}</H7>
+      </Placeholder.Line>
+    </ByLineWrapper>
   </Tile>
 ));
 
