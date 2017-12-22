601e6432a1a74edfce5b84bc1ad675e26aab59ab
Marcelo Reyna <Lepozepo@gmail.com>
2017-12-22 08:50:41 -0800

    Add withToggleLike mutation

--- src/@data/likes/index.js
+++ src/@data/likes/index.js
@@ -1,2 +1,3 @@
 export { default as withProfileLikes } from './withProfileLikes';
 export { default as withProfileRecentLikes } from './withProfileRecentLikes';
+export { default as withToggleLike } from './withToggleLike';
