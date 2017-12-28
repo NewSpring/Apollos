b017c627b9fff689f06df8b831c104830f5377f8
Conrad VanLandingham <conrad@landdolph.in>
2017-12-28 10:45:14 -0600

    Fix linter error

--- src/group-finder/Filter.js
+++ src/group-finder/Filter.js
@@ -1,6 +1,6 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
-import { Animated, StyleSheet, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { compose } from 'recompose';
 import { get, without } from 'lodash';
 
