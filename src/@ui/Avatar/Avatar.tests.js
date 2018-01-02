26c38103ce1009f12dfdd1427ca875ea5e227731
Conrad VanLandingham <hi@convan.me>
2018-01-02 09:16:11 -0600

    fix linter error

--- src/@ui/Avatar/Avatar.tests.js
+++ src/@ui/Avatar/Avatar.tests.js
@@ -3,6 +3,7 @@ import renderer from 'react-test-renderer';
 import { ThemeProvider } from '@ui/theme';
 
 import Avatar from './';
+
 const source = { url: 'https://dg0ddngxdz549.cloudfront.net/images/cached/images/remote/http_s3.amazonaws.com/ns.images/all/member_images/members.nophoto_1000_1000_90_c1.jpg' };
 
 describe('The Avatar component', () => {
