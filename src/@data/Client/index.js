86922847c13965d8a5f6f9ba2f2c518da03ab2fd
Marcelo Reyna <Lepozepo@gmail.com>
2018-01-02 13:39:22 -0800

    Add quick notes
    We can make this easier later

--- src/@data/Client/index.js
+++ src/@data/Client/index.js
@@ -16,6 +16,8 @@ export default new ApolloClient({
       return defaultDataIdFromObject(obj);
     },
     fragmentMatcher: new IntrospectionFragmentMatcher({
+      // NOTE: Introspection result goes here!
+      // More Info: ./README.md
       introspectionQueryResultData: JSON.parse('{"__schema":{"types":[{"kind":"INTERFACE","name":"Node","possibleTypes":[{"name":"User"},{"name":"Content"},{"name":"Campus"},{"name":"Location"},{"name":"File"},{"name":"Navigation"},{"name":"Person"},{"name":"PhoneNumber"},{"name":"Group"},{"name":"DefinedValue"},{"name":"GroupSchedule"},{"name":"GroupMember"},{"name":"GroupLocation"},{"name":"Attribute"},{"name":"AttributeValue"},{"name":"SavedPayment"},{"name":"PaymentDetail"},{"name":"Transaction"},{"name":"TransactionDetail"},{"name":"FinancialAccount"},{"name":"ScheduledTransaction"},{"name":"BinaryFile"}]},{"kind":"INTERFACE","name":"MutationResponse","possibleTypes":[{"name":"PhoneNumberMutationResponse"},{"name":"DeviceRegistrationMutationResponse"},{"name":"AttributeValueMutationResponse"},{"name":"OrderMutationResponse"},{"name":"CompleteOrderMutationResponse"},{"name":"ValidateMutationResponse"},{"name":"SavePaymentMutationResponse"},{"name":"ScheduledTransactionMutationResponse"},{"name":"StatementMutationResponse"},{"name":"GroupsMutationResponse"},{"name":"LikesMutationResponse"}]}]}}'),
     }),
   }),
