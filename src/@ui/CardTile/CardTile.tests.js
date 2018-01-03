1189c2f0821f6378a71fd75ca92e6b424ec6b320
Burke Shartsis <burke@burkeshartsis.com>
2018-01-03 12:56:43 -0500

    updated tests.

--- src/@ui/CardTile/CardTile.tests.js
+++ src/@ui/CardTile/CardTile.tests.js
@@ -30,4 +30,16 @@ describe('the Card component', () => {
     );
     expect(tree).toMatchSnapshot();
   });
+  it('should not render a number box if prop is missing', () => {
+    const tree = renderer.create(
+      <ThemeProvider>
+        <CardTile
+          title={'Sermon Title'}
+          byLine={'Marty McFly'}
+          date={'3mo'}
+        />
+      </ThemeProvider>,
+    );
+    expect(tree).toMatchSnapshot();
+  });
 });
