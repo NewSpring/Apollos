fe96ad885256f4d609dc13000dad77d68905f068
Marcelo Reyna <Lepozepo@gmail.com>
2018-01-03 13:41:25 -0800

    Patch up to work without mutating

--- src/@data/utils/fetchMoreResolver.js
+++ src/@data/utils/fetchMoreResolver.js
@@ -1,28 +1,19 @@
 import get from 'lodash/get';
-import set from 'lodash/set';
+import set from 'lodash/fp/set';
 
 // TODO: Test this
 export default function fetchMoreResolver({ collectionName, mapTo, data } = {}) {
   return () => {
-    // console.log(get(data, collectionName, []).length);
     data.fetchMore({
       variables: { ...data.variables, skip: get(data, collectionName, []).length },
       updateQuery: (previousResult, { fetchMoreResult }) => {
-        // console.log({ previousResult, fetchMoreResult });
         if (!fetchMoreResult) return previousResult;
-        // console.log(set({ ...previousResult },
-        //   mapTo || collectionName,
-        //   [
-        //     ...get(previousResult, collectionName),
-        //     ...get(fetchMoreResult, collectionName),
-        //   ],
-        // ));
-        return set({ ...previousResult },
-          mapTo || collectionName,
+        return set(mapTo || collectionName,
           [
             ...get(previousResult, collectionName),
             ...get(fetchMoreResult, collectionName),
           ],
+          { ...previousResult },
         );
       },
     });
