80aa885fbf0348ec742ef22725c84a8ee33c2653
Marcelo Reyna <Lepozepo@gmail.com>
2018-01-11 13:10:59 -0800

    Add withSavedPaymentMethod
    Requires Heighliner update

---
+++ src/@data/withSavedPaymentMethod/index.js
@@ -0,0 +1,33 @@
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
+import get from 'lodash/get';
+
+export const QUERY = gql`
+  query SavedPayment($id: ID!) {
+    savedPaymentMethod: savedPayment(id: $id) {
+      id: entityId
+      name
+      payment {
+        accountNumber
+        paymentType
+      }
+    }
+  }
+`;
+
+export default graphql(QUERY, {
+  props: ({ data: { savedPaymentMethod, loading } }) => ({
+    savedPaymentMethod: {
+      ...savedPaymentMethod,
+      paymentType: get(savedPaymentMethod, 'payment.paymentType') === 'ACH' ? 'bankAccount' : 'creditCard',
+      accountNumber: get(savedPaymentMethod, 'payment.accountNumber'),
+    },
+    isLoading: loading,
+  }),
+  options: (ownProps = {}) => ({
+    variables: {
+      id: ownProps.id,
+    },
+  }),
+});
+
