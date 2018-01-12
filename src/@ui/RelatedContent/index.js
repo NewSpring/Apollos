a82469c118ab9570a09dba502c1c34549ad35ee9
Burke Shartsis <burke@burkeshartsis.com>
2018-01-12 15:00:39 -0500

    completed RelatedContent component 💥

--- src/@ui/RelatedContent/index.js
+++ src/@ui/RelatedContent/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { View } from 'react-native';
 import PropTypes from 'prop-types';
 import { compose, pure, branch, withProps, setPropTypes, defaultProps } from 'recompose';
+import { get, times } from 'lodash';
 
 import withRelatedContent from '@data/withRelatedContent';
 import { Link } from '@ui/NativeWebRouter';
@@ -21,15 +22,21 @@ const generateLoadingStateData = (numberOfItems = 1) => {
     isLoading: true,
   });
 
-  const loadingStateData = [itemData()];
-
-  while (loadingStateData.length < numberOfItems) {
+  const loadingStateData = { taggedContent: [] };
+  console.log('BOOM!');
+  times(numberOfItems, (n) => {
     const newData = itemData();
-    newData.id = `fakeId${loadingStateData.length}`;
-    loadingStateData.push(newData);
-  }
+    newData.id = `fakeId${n}`;
+    loadingStateData.taggedContent.push(newData);
+  });
 
   return loadingStateData;
+};
+
+const getItemImages = (item) => {
+  let images = get(item, 'content.images', []);
+  if (!images.length) images = get(item, 'parent.content.images', [{}]);
+  return images[0].url; // TODO short circuit. Highliner should handle image selection in the array
 };
 
 const enhance = compose(
@@ -44,24 +51,22 @@ const enhance = compose(
     isLoading: false,
     sectionTitle: 'More Like This',
   }),
-  branch(({ isLoading, data }) => isLoading && !data.length, withProps({
+  branch(({ isLoading, data }) => (isLoading, !data.length), withProps({ // eslint-disable-line
     data: generateLoadingStateData(3),
     fetchMore: false,
   })),
 );
 
-const renderItems = (taggedContent) => {
-  // console.log(taggedContent);
+const renderItems = (data) => {
   let items = null;
-
-  if (taggedContent !== undefined) {
-    items = taggedContent.map(item => (
-      <Link to={getLinkPath(item)}>
+  if (data.taggedContent !== undefined) {
+    items = data.taggedContent.map(item => (
+      <Link to={getLinkPath(item)} key={item.id}>
         <ThumbnailCard
           title={item.title}
           category={item.channelName}
-          image={'https://picsum.photos/400/400/?random'}
-          key={item.id}
+          image={getItemImages(item)}
+          isLoading={item.isLoading}
         />
       </Link>
     ));
@@ -72,21 +77,21 @@ const renderItems = (taggedContent) => {
 
 const Wrapper = styled(({ theme }) => ({
   paddingVertical: theme.sizing.baseUnit,
+  backgroundColor: theme.colors.lightSecondary,
 }))(View);
 
 const Title = styled(({ theme }) => ({
   textAlign: 'center',
-  paddingBottom: theme.sizing.baseUnite,
+  marginBottom: theme.sizing.baseUnit / 2,
 }))(H5);
 
 const RelatedContent = enhance(({
   sectionTitle,
   data,
-  isLoading,
 }) => (
   <Wrapper>
-    <Title><H5>{sectionTitle}</H5></Title>
-    {renderItems(data.taggedContent)}
+    <Title>{sectionTitle}</Title>
+    <View>{renderItems(data)}</View>
   </Wrapper>
 ));
 
