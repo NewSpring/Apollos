e4ae58cda813c8f044bfcbdabbf2995a628a1709
Burke Shartsis <burke@burkeshartsis.com>
2018-01-15 16:23:19 -0500

    WIP refactor for better testing flow 💪💥

--- src/@ui/RelatedContent/index.js
+++ src/@ui/RelatedContent/index.js
@@ -1,100 +1,31 @@
-import React from 'react';
-import { View } from 'react-native';
 import PropTypes from 'prop-types';
-import { compose, pure, branch, withProps, setPropTypes, defaultProps } from 'recompose';
-import { get, times } from 'lodash';
+import { compose, pure, branch, setPropTypes, defaultProps, renderNothing } from 'recompose';
 
 import withRelatedContent from '@data/withRelatedContent';
-import { Link } from '@ui/NativeWebRouter';
-import { getLinkPath } from '@utils/content';
-import { withThemeMixin } from '@ui/theme';
-import styled from '@ui/styled';
-import { H5 } from '@ui/typography';
-import ThumbnailCard from '@ui/ThumbnailCard';
 
-const generateLoadingStateData = (numberOfItems = 1) => {
-  const itemData = () => ({
-    id: 'fakeId0',
-    title: '',
-    channelName: '',
-    content: {
-      images: [],
-    },
-    isLoading: true,
-  });
-
-  const loadingStateData = { taggedContent: [] };
-  times(numberOfItems, (n) => {
-    const newData = itemData();
-    newData.id = `fakeId${n}`;
-    loadingStateData.taggedContent.push(newData);
-  });
-
-  return loadingStateData;
-};
-
-const getItemImages = (item) => {
-  let images = get(item, 'content.images', []);
-  if (!images.length) images = get(item, 'parent.content.images', [{}]);
-  return images[0].url; // TODO short circuit. Highliner should handle image selection in the array
-};
+import RelatedContentWithoutData from './RelatedContentWithoutData';
 
 const enhance = compose(
   pure,
   setPropTypes({
-    excludedIds: PropTypes.arrayOf(PropTypes.string),
-    tags: PropTypes.arrayOf(PropTypes.string),
+    excludedIds: PropTypes.arrayOf(PropTypes.string).isRequired,
+    tags: PropTypes.arrayOf(PropTypes.string).isRequired,
+    sectionTitle: PropTypes.string,
     isLoading: PropTypes.bool,
   }),
   defaultProps({
     isLoading: false,
-    sectionTitle: 'More Like This',
+    excludedIds: [],
+    tags: [],
   }),
+  // TODO: Condsider refactoring the branches below!
+  // only run query if we have required data!
+  branch(({ tags, excludedIds, data }) => console.log('BOOM 1', data) || (!tags.length || !excludedIds.length || data !== 'undefined'), renderNothing),
   withRelatedContent,
-  branch(({ isLoading, data }) => (isLoading && data.taggedContent !== 'undefined'), withProps({
-    data: generateLoadingStateData(3),
-  })),
-  withThemeMixin(() => ({
-    type: 'light',
-  })),
+  // only render if query returns data!
+  branch(({ data }) => console.log('BOOM 3', data) || (data.taggedContent === 'undefined'), renderNothing),
 );
 
-const renderItems = (data) => {
-  let items = null;
-  if (data.taggedContent !== undefined) {
-    items = data.taggedContent.map(item => (
-      <Link to={getLinkPath(item)} key={item.id}>
-        <ThumbnailCard
-          title={item.title}
-          category={item.channelName}
-          image={getItemImages(item)}
-          isLoading={item.isLoading}
-        />
-      </Link>
-    ));
-  }
-
-  return items;
-};
-
-const Wrapper = styled(({ theme }) => ({
-  paddingVertical: theme.sizing.baseUnit,
-  backgroundColor: theme.colors.lightSecondary,
-}))(View);
-
-const Title = styled(({ theme }) => ({
-  textAlign: 'center',
-  marginBottom: theme.sizing.baseUnit / 2,
-}))(H5);
-
-const RelatedContent = enhance(({
-  sectionTitle,
-  data,
-}) => (
-  <Wrapper>
-    <Title>{sectionTitle}</Title>
-    {renderItems(data)}
-  </Wrapper>
-));
+const RelatedContent = enhance(RelatedContentWithoutData);
 
 export default RelatedContent;
