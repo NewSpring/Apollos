acde07193fbc2cd96d45d90e58ca42400b34b8c8
Marcelo Reyna <Lepozepo@gmail.com>
2018-01-16 13:47:35 -0800

    Validate cards without noise

--- src/@data/withGive/createValidationOrder.js
+++ src/@data/withGive/createValidationOrder.js
@@ -1,7 +1,8 @@
 import { graphql } from 'react-apollo';
+import isEmpty from 'lodash/isEmpty';
 import Client from '@data/Client';
+import removeUndefined from '@utils/removeUndefined';
 import { QUERY as contributionsQuery } from './withContributions';
-import getOrderDetails from './selectors/getOrderDetails';
 import { MUTATION } from './createOrder';
 
 // NOTE: They create order after capturing a billing address
@@ -12,8 +13,20 @@ export default graphql(MUTATION, {
       const { contributions: state } = Client.readQuery({
         query: contributionsQuery,
       });
-      const orderDetails = getOrderDetails(state);
-      orderDetails.amount = 0;
+      const orderDetails = removeUndefined({
+        billing: {
+          'first-name': state.firstName,
+          'last-name': state.lastName,
+          email: state.email,
+          address1: state.street1,
+          address2: state.street2,
+          city: state.city,
+          state: state.state,
+          postal: state.zipCode,
+        },
+        'merchant-defined-field-2': isEmpty(state.campusId) ? undefined : state.campusId,
+        amount: 0,
+      });
 
       return mutate({
         variables: {
