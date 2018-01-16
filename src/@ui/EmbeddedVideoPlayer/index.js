f192f393e5fb2ec5a33ddebb547f67d2febad34e
Burke Shartsis <burke@burkeshartsis.com>
2018-01-16 14:08:10 -0500

    fixed unnecessry rerenders 💥

--- src/@ui/EmbeddedVideoPlayer/index.js
+++ src/@ui/EmbeddedVideoPlayer/index.js
@@ -1,20 +1,30 @@
 import React from 'react';
+import { compose, pure, setPropTypes } from 'recompose';
 import PropTypes from 'prop-types';
+
 import WebView from './WebView';
 
-const EmbeddedVideoPlayer = ({ src, ...otherProps }) => (
+const enhance = compose(
+  pure,
+  setPropTypes({
+    src: PropTypes.string,
+  }),
+);
+
+const buildSourceObject = source => ({ uri: source });
+
+const EmbeddedVideoPlayer = enhance(({
+  src,
+  ...otherProps
+}) => (
   <WebView
-    source={{ uri: src }}
+    source={buildSourceObject(src)}
     mediaPlaybackRequiresUserAction={false}
     allowsInlineMediaPlayback
     bounces={false}
     scrollEnabled={false}
     {...otherProps}
   />
-);
-
-EmbeddedVideoPlayer.propTypes = {
-  src: PropTypes.string,
-};
+));
 
 export default EmbeddedVideoPlayer;
