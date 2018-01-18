ec46cccdf7dd6e25c4224528db4bdae6ae8166e7
Marcelo Reyna <Lepozepo@gmail.com>
2018-01-18 10:08:55 -0800

    Build dashboard subheader

---
+++ src/@ui/DashboardSubheader/index.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import {
+  View,
+} from 'react-native';
+import PropTypes from 'prop-types';
+import { H6, H7 } from '@ui/typography';
+import styled from '@ui/styled';
+import Button from '@ui/Button';
+
+const Row = styled(({ theme }) => ({
+  flexDirection: 'row',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  paddingHorizontal: theme.sizing.baseUnit / 2,
+}))(View);
+
+const StyledH6 = styled(({ theme }) => ({
+  color: theme.colors.darkTertiary,
+}))(H6);
+
+const StyledH7 = styled(({ theme }) => ({
+  color: theme.colors.darkTertiary,
+}))(H7);
+
+const StyledButton = styled(({ theme }) => ({
+  color: theme.colors.darkTertiary,
+  borderColor: theme.colors.darkTertiary,
+  paddingVertical: theme.sizing.baseUnit / 6,
+  paddingHorizontal: theme.sizing.baseUnit,
+  borderWidth: 1,
+}))(Button);
+
+function DashboardSubheader(props) {
+  return (
+    <Row>
+      <StyledH6>{props.text}</StyledH6>
+      <StyledButton
+        bordered
+        pill
+        onPress={props.onPress}
+      >
+        <StyledH7>{props.buttonText}</StyledH7>
+      </StyledButton>
+    </Row>
+  );
+}
+
+DashboardSubheader.propTypes = {
+  text: PropTypes.string,
+  buttonText: PropTypes.string,
+  onPress: PropTypes.func,
+};
+
+DashboardSubheader.defaultProps = {
+  text: '',
+  buttonText: '',
+  onPress() {},
+};
+
+export default DashboardSubheader;
