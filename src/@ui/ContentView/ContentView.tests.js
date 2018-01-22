7b5f8f3b75c3875df828e21437b5688faaf5fbd2
Burke Shartsis <burke@burkeshartsis.com>
2018-01-22 10:05:35 -0500

    Content header gradients (#227)
    * WIP refactored ImageGradient component
    
    * ditched the overly complicated ImageGradient component in favor of a more direct solve. Series and Studies now have header gradients over their images 💥
    
    * adding tests for ContentMedia via content view 💪

--- src/@ui/ContentView/ContentView.tests.js
+++ src/@ui/ContentView/ContentView.tests.js
@@ -17,4 +17,47 @@ describe('the ContentView component', () => {
     );
     expect(tree).toMatchSnapshot();
   });
+  it('renders with images', () => {
+    const images = [
+      {
+        uri: 'https://picsum.photos/600/400/?random',
+        width: 600,
+        height: 400,
+      },
+      {
+        uri: 'https://picsum.photos/600/400/?random',
+        width: 600,
+        height: 400,
+      },
+    ];
+
+    const tree = renderer.create(
+      <Providers>
+        <ContentView images={images} imageOverlayColor={'salmon'}>
+          <Title>The great escape</Title>
+          <ByLine authors={['Mike Douglas']} />
+          <HTMLView>{'<p>Yo yo</p><p>this is some cool html</p>'}</HTMLView>
+        </ContentView>
+      </Providers>,
+    );
+    expect(tree).toMatchSnapshot();
+  });
+  it('renders with video', () => {
+    const videoSource = {
+      video: {
+        embedUrl: 'https://player.ooyala.com/static/v4/production/latest/skin-plugin/iframe.html?ec=h2anlxYzE6G8splCI1Fi-VQciOUJ-fl8&pbid=ZmJmNTVlNDk1NjcwYTVkMzAzODkyMjg0&pcode=&pcode=E1dWM6UGncxhent7MRATc3hmkzUD&skin.config=https%3A%2F%2Fdl.dropbox.com%2Fs%2Fsodcv1d9a4ezwm4%2Fskin.new.json%3Fdl%3D1',
+      },
+    };
+
+    const tree = renderer.create(
+      <Providers>
+        <ContentView video={videoSource}>
+          <Title>The great escape</Title>
+          <ByLine authors={['Mike Douglas']} />
+          <HTMLView>{'<p>Yo yo</p><p>this is some cool html</p>'}</HTMLView>
+        </ContentView>
+      </Providers>,
+    );
+    expect(tree).toMatchSnapshot();
+  });
 });
