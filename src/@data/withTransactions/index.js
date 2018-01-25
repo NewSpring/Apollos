5df80f5e48b4e8cc6110f967af39eee426cac650
Marcelo Reyna <Lepozepo@gmail.com>
2018-01-25 10:35:27 -0800

    Set up initial render

--- src/@data/withTransactions/index.js
+++ src/@data/withTransactions/index.js
@@ -1,6 +1,7 @@
 import { graphql } from 'react-apollo';
-import fetchMoreResolver from '@data/utils/fetchMoreResolver';
 import gql from 'graphql-tag';
+import moment from 'moment';
+import fetchMoreResolver from '@data/utils/fetchMoreResolver';
 
 export const QUERY = gql`
   query GetTransactions($limit: Int, $skip: Int, $people: [Int], $start: String, $end: String) {
@@ -16,7 +17,7 @@ export const QUERY = gql`
       date
       status
       summary
-      person { firstName, lastName, photo }
+      person { id, firstName, lastName, photo }
       details {
         id
         amount
@@ -33,8 +34,14 @@ export default graphql(QUERY, {
       skip: ownProps.skip || 0,
     },
   }),
-  props: ({ ownProps, data } = {}) => console.log(data, ownProps) || ({
-    transactions: data.transactions,
+  props: ({ ownProps, data } = {}) => ({
+    transactions: data.transactions && data.transactions.map(transaction => ({
+      ...transaction,
+      // NOTE: Holtzman logic, this should be in Heighliner
+      year: moment(transaction.date).utc().year(),
+      details: transaction.details && transaction.details
+        .filter(x => (x.amount && Number(x.amount) !== 0)),
+    })),
     isLoading: ownProps.isLoading || data.loading,
     fetchMore: fetchMoreResolver({
       collectionName: 'content',
--- src/give/ContributionHistory.js
+++ src/give/ContributionHistory.js
@@ -1,11 +1,59 @@
-import React from 'react';
-import Header from '@ui/Header';
+import React, { PureComponent } from 'react';
+import {
+  ScrollView,
+  View,
+} from 'react-native';
+import PropTypes from 'prop-types';
+import { compose, branch, renderComponent } from 'recompose';
+import { map, groupBy } from 'lodash';
+import ActivityIndicator from '@ui/ActivityIndicator';
 import FlexedView from '@ui/FlexedView';
+import HistoricalContributionCard from '@ui/HistoricalContributionCard';
+import withTransactions from '@data/withTransactions';
+import { H5 } from '@ui/typography';
 
-export default function ContributionHistory() {
-  return (
-    <FlexedView>
-      <Header titleText="Contribution History" />
-    </FlexedView>
-  );
+class ContributionHistory extends PureComponent {
+  static propTypes = {
+    transactions: PropTypes.arrayOf(PropTypes.shape({})),
+  };
+
+  static defaultProps = {
+    transactions: [],
+  };
+
+  render() {
+    const transactionsPerYear = groupBy(this.props.transactions, 'year');
+    return (
+      <FlexedView>
+        <ScrollView>
+          {map(transactionsPerYear, (groupedTransactions, year) => (
+            <View>
+              <H5>{year}</H5>
+              {groupedTransactions.map(transaction => (
+                <View>
+                  {transaction.details.map(detail => (
+                    <HistoricalContributionCard
+                      key={detail.id}
+                      amount={detail.amount}
+                      fundName={detail.account.name}
+                      contributorName={`${transaction.person.firstName} ${transaction.person.lastName}`}
+                      date={transaction.date}
+                      profileImageUrl="via.placeholder.com/50x50"
+                    />
+                  ))}
+                </View>
+              ))}
+            </View>
+          ))}
+        </ScrollView>
+      </FlexedView>
+    );
+  }
 }
+
+const enhance = compose(
+  withTransactions,
+  branch(({ isLoading }) => isLoading, renderComponent(ActivityIndicator)),
+);
+
+export default enhance(ContributionHistory);
