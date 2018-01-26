093c6325261e79866007191114065f64dacbb6b8
Conrad VanLandingham <hi@convan.me>
2018-01-26 11:14:44 -0600

    Use TableView on topic toggles

--- src/tabs/Profile/Topics.js
+++ src/tabs/Profile/Topics.js
@@ -1,29 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { StyleSheet, ScrollView } from 'react-native';
+import { ScrollView } from 'react-native';
 import { compose, pure } from 'recompose';
 import PaddedView from '@ui/PaddedView';
-import { withTheme } from '@ui/theme';
 import { Switch } from '@ui/inputs';
 import { H7 } from '@ui/typography';
 import styled from '@ui/styled';
 import withTopics from '@data/withTopics';
 import withUser from '@data/withUser';
-
-const TabInfo = styled(({ theme }) => ({
-  backgroundColor: theme.colors.background.overlay,
-}))(PaddedView);
+import TableView, { Cell, CellText, Divider } from '@ui/TableView';
 
 const TabInfoText = styled({ textAlign: 'center' })(H7);
-
-const SwitchRow = withTheme(({ theme }) => ({
-  wrapperStyle: {
-    marginVertical: 0,
-    paddingVertical: theme.sizing.baseUnit / 3,
-    borderBottomWidth: StyleSheet.hairlineWidth,
-    borderBottomColor: theme.colors.shadows.default,
-  },
-}))(Switch);
 
 const enhance = compose(
   pure,
@@ -37,21 +24,23 @@ const Topics = enhance(({
   toggleTopic,
 }) => (
   <ScrollView>
-    <TabInfo>
+    <PaddedView>
       <TabInfoText>
         Personalize your NewSpring Home and follow the types of content you care about.
       </TabInfoText>
-    </TabInfo>
-    <PaddedView>
-      {topics.map(topic => (
-        <SwitchRow
-          key={topic}
-          label={topic}
-          value={followedTopics.indexOf(topic) > -1}
-          onValueChange={() => toggleTopic(topic)}
-        />
-      ))}
     </PaddedView>
+    <TableView>
+      {topics.map((topic, i) => [
+        <Cell key={topic}>
+          <CellText>{topic}</CellText>
+          <Switch
+            value={followedTopics.indexOf(topic) > -1}
+            onValueChange={() => toggleTopic(topic)}
+          />
+        </Cell>,
+        (i !== topics.length - 1) ? <Divider key={`${topic}-divider`} /> : null,
+      ])}
+    </TableView>
   </ScrollView>
 ));
 
