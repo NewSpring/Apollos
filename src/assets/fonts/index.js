e55c7a72093116fe8a1d5abafa3e4e549e90ab94
Burke Shartsis <burke@burkeshartsis.com>
2018-01-25 23:12:22 -0500

    WIP custom font loading

--- src/assets/fonts/index.js
+++ src/assets/fonts/index.js
@@ -1,6 +1,59 @@
+/* eslint-disable import/no-unresolved */
 export default [
-  // {
-  //   name: 'OpenSans',
-  //   asset: require('./OpenSans/OpenSans.ttf'),
-  // },
+  {
+    name: 'Colfax-Light',
+    asset: 'https://d3n6tjerleuu41.cloudfront.net/fonts/colfax/Colfax-Light.otf',
+  },
+  {
+    name: 'Colfax-LightItalic',
+    asset: 'https://d3n6tjerleuu41.cloudfront.net/fonts/colfax/Colfax-LightItalic.otf',
+  },
+  {
+    name: 'Colfax-Regular',
+    asset: 'https://d3n6tjerleuu41.cloudfront.net/fonts/colfax/Colfax-Regular.otf',
+  },
+  {
+    name: 'Colfax-RegularItalic',
+    asset: 'https://d3n6tjerleuu41.cloudfront.net/fonts/colfax/Colfax-RegularItalic.otf',
+  },
+  {
+    name: 'Colfax-Medium',
+    asset: 'https://d3n6tjerleuu41.cloudfront.net/fonts/colfax/Colfax-Medium.otf',
+  },
+  {
+    name: 'Colfax-MediumItalic',
+    asset: 'https://d3n6tjerleuu41.cloudfront.net/fonts/colfax/Colfax-MediumItalic.otf',
+  },
+  {
+    name: 'Colfax-Bold',
+    asset: 'https://d3n6tjerleuu41.cloudfront.net/fonts/colfax/Colfax-Bold.otf',
+  },
+  {
+    name: 'Colfax-BoldItalic',
+    asset: 'https://d3n6tjerleuu41.cloudfront.net/fonts/colfax/Colfax-BoldItalic.otf',
+  },
+  {
+    name: 'Colfax-Black',
+    asset: 'https://d3n6tjerleuu41.cloudfront.net/fonts/colfax/Colfax-Black.otf',
+  },
+  {
+    name: 'Colfax-BlackItalic',
+    asset: 'https://d3n6tjerleuu41.cloudfront.net/fonts/colfax/Colfax-BlackItalic.otf',
+  },
+  {
+    name: 'DroidSerif-Regular',
+    asset: 'https://d3n6tjerleuu41.cloudfront.net/fonts/droid-serif/DroidSerif-Regular.ttf',
+  },
+  {
+    name: 'DroidSerif-Bold',
+    asset: 'https://d3n6tjerleuu41.cloudfront.net/fonts/droid-serif/DroidSerif-Bold.ttf',
+  },
+  {
+    name: 'DroidSerif-Italic',
+    asset: 'https://d3n6tjerleuu41.cloudfront.net/fonts/droid-serif/DroidSerif-Italic.ttf',
+  },
+  {
+    name: 'DroidSerif-BoldItalic',
+    asset: 'https://d3n6tjerleuu41.cloudfront.net/fonts/droid-serif/DroidSerif-BoldItalic.ttf',
+  },
 ];
