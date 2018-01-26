e7707367c2acbfd4d47e27f67ffd1f505a28e0e6
Burke Shartsis <burke@burkeshartsis.com>
2018-01-26 14:11:23 -0500

    fixing typo in story

--- src/@ui/typography/H6/H6.stories.js
+++ src/@ui/typography/H6/H6.stories.js
@@ -7,5 +7,5 @@ import H6 from './';
 
 storiesOf('@ui/typography', module)
   .add('H6', () => (
-    <H6>{'"Radical obedience to Christ is not easy... It\'s not comfort, not health, not wealth, and not prosperity in this world. Radical obedience to Christ risks losing all these things. But in the end, such risk finds its reward in Christ. And he is more than enough for us." ― David Platt'}</H6>
+    <H6>{'"Radical obedience to Christ is not easy... It\'s not comfort, not health, not wealth, and not prosperity in this world. Radical obedience to Christ risks losing all these things. But in the end, such risk finds its reward in Christ. And He is more than enough for us." ― David Platt'}</H6>
   ));
