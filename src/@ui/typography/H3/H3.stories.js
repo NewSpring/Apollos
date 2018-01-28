946d635fd9d7b3e58ebeea2593395b41dbae18ba
Burke Shartsis <burke@burkeshartsis.com>
2018-01-27 21:31:50 -0500

    updating H3 to use new theme fonts.

--- src/@ui/typography/H3/H3.stories.js
+++ src/@ui/typography/H3/H3.stories.js
@@ -5,7 +5,14 @@ import { storiesOf } from '@storybook/react-native';
 
 import H3 from './';
 
-storiesOf('@ui/typography', module)
-  .add('H3', () => (
+storiesOf('@ui/typography/H3', module)
+  .add('Default', () => (
     <H3>{'"The Christian shoemaker does his duty not by putting little crosses on the shoes, but by making good shoes, because God is interested in good craftsmanship." ― Martin Luther'}</H3>
-  ));
+  ))
+  .add('Border Box – platform testing', () => {
+    const border = { borderWidth: 1 };
+
+    return (
+      <H3 style={border}>Heading 3</H3>
+    );
+  });
--- src/@ui/typography/H3/index.js
+++ src/@ui/typography/H3/index.js
@@ -6,9 +6,8 @@ import { withPlaceholder, Typography } from '@ui/Placeholder';
 
 const styles = styled(({ theme }) => ({
   fontSize: theme.helpers.rem(1.8),
-  fontFamily: theme.typography.fontFamilySans,
+  fontFamily: theme.typography.fontFamilySansNeue.bold.default,
   lineHeight: theme.helpers.verticalRhythm(1.8, 1.145),
-  fontWeight: '700',
   color: theme.colors.text.primary,
 }), 'H3');
 
