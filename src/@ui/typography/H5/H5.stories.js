204376d3ebc12baba91138b3a946d7f7b6596d24
Burke Shartsis <burke@burkeshartsis.com>
2018-01-27 21:56:35 -0500

    updating H5 to use new theme fonts

--- src/@ui/typography/H5/H5.stories.js
+++ src/@ui/typography/H5/H5.stories.js
@@ -5,7 +5,14 @@ import { storiesOf } from '@storybook/react-native';
 
 import H5 from './';
 
-storiesOf('@ui/typography', module)
-  .add('H5', () => (
+storiesOf('@ui/typography/H5', module)
+  .add('Default', () => (
     <H5>{'"Faith does not eliminate questions. But faith knows where to take them." ― Elisabeth Elliot'}</H5>
-  ));
+  ))
+  .add('Border Box – platform testing', () => {
+    const border = { borderWidth: 1 };
+
+    return (
+      <H5 style={border}>Heading 5</H5>
+    );
+  });
