373d5fab47f7ae73d749ecd9b27bf434148b9551
Burke Shartsis <burke@burkeshartsis.com>
2018-01-27 22:03:07 -0500

    updating H6 to use new theme fonts

--- src/@ui/typography/H6/H6.stories.js
+++ src/@ui/typography/H6/H6.stories.js
@@ -5,7 +5,14 @@ import { storiesOf } from '@storybook/react-native';
 
 import H6 from './';
 
-storiesOf('@ui/typography', module)
-  .add('H6', () => (
+storiesOf('@ui/typography/H6', module)
+  .add('Default', () => (
     <H6>{'"Radical obedience to Christ is not easy... It\'s not comfort, not health, not wealth, and not prosperity in this world. Radical obedience to Christ risks losing all these things. But in the end, such risk finds its reward in Christ. And He is more than enough for us." ― David Platt'}</H6>
-  ));
+  ))
+  .add('Border Box – platform testing', () => {
+    const border = { borderWidth: 1 };
+
+    return (
+      <H6 style={border}>Heading 6</H6>
+    );
+  });
--- src/@ui/typography/H6/index.js
+++ src/@ui/typography/H6/index.js
@@ -6,9 +6,8 @@ import { withPlaceholder, Typography } from '@ui/Placeholder';
 
 const styles = styled(({ theme }) => ({
   fontSize: theme.helpers.rem(0.875),
-  fontFamily: theme.typography.fontFamilySans,
+  fontFamily: theme.typography.fontFamilySansNeue.bold.default,
   lineHeight: theme.helpers.verticalRhythm(0.875, 1.02),
-  fontWeight: '700',
   color: theme.colors.text.primary,
 }), 'H6');
 
