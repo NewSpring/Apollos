f3f0438b1c330fc1ed793e28441a25f2020c8f33
Burke Shartsis <burke@burkeshartsis.com>
2018-01-29 14:00:48 -0500

    adjusting H3 line height and padding to account for how ios and android render fonts differently 😫😣

--- src/@ui/typography/H3/H3.stories.js
+++ src/@ui/typography/H3/H3.stories.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { View } from 'react-native';
 import { storiesOf } from '@storybook/react-native';
 
 
@@ -14,5 +14,15 @@ storiesOf('@ui/typography/H3', module)
 
     return (
       <H3 style={border}>Heading 3</H3>
+    );
+  })
+  .add('Border Box – platform testing', () => {
+    const border = { borderWidth: 1 };
+
+    return (
+      <View>
+        <H3 style={border}>Heading 3</H3>
+        <H3 style={border}>{'"The Christian shoemaker does his duty not by putting little crosses on the shoes, but by making good shoes, because God is interested in good craftsmanship." ― Martin Luther'}</H3>
+      </View>
     );
   });
