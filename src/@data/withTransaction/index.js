590a0d75b63d592c36834c6883156c41e8eaf098
Marcelo Reyna <Lepozepo@gmail.com>
2018-01-30 10:46:54 -0800

    Patch withTransaction

--- src/@data/withTransaction/index.js
+++ src/@data/withTransaction/index.js
@@ -1,9 +1,51 @@
 import { graphql } from 'react-apollo';
-import transactionQuery from './transactionQuery';
+import gql from 'graphql-tag';
+import get from 'lodash/get';
 
-export default graphql(transactionQuery, {
-  props: ({ data: { content } }) => ({
-    content,
+export const QUERY = gql`
+  query GetTransaction($id: ID!) {
+    transaction: node(id: $id) {
+      id
+      ... on Transaction {
+        id
+        date
+        summary
+        status
+        person {
+          firstName
+          nickName
+          lastName
+        }
+        details {
+          id
+          amount
+          account {
+            name
+            description
+            summary
+            end
+            start
+          }
+        }
+        payment {
+          id
+          paymentType
+          accountNumber
+        }
+      }
+    }
+  }
+`;
+
+export default graphql(QUERY, {
+  props: ({ data: { transaction } }) => ({
+    transaction: {
+      ...transaction,
+      payment: {
+        ...get(transaction, 'payment'),
+        paymentMethod: get(transaction, 'payment.paymentType', '') === 'ACH' ? 'bankAccount' : 'creditCard',
+      },
+    },
   }),
   options: (ownProps = {}) => ({
     variables: {
