31b869620af6ab2bcbb85ce4a78daa32e6b59749
Burke Shartsis <burke@burkeshartsis.com>
2018-01-30 10:49:54 -0500

    adjusting h6 lineheight and padding to render consistantly across all platforms 😬

--- src/@ui/typography/H6/H6.stories.js
+++ src/@ui/typography/H6/H6.stories.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { View } from 'react-native';
 import { storiesOf } from '@storybook/react-native';
 
 
@@ -10,9 +10,12 @@ storiesOf('@ui/typography/H6', module)
     <H6>{'"Radical obedience to Christ is not easy... It\'s not comfort, not health, not wealth, and not prosperity in this world. Radical obedience to Christ risks losing all these things. But in the end, such risk finds its reward in Christ. And He is more than enough for us." ― David Platt'}</H6>
   ))
   .add('Border Box – platform testing', () => {
-    const border = { borderWidth: 1 };
+    const border = { borderWidth: 1, borderStyle: 'solid' };
 
     return (
-      <H6 style={border}>Heading 6</H6>
+      <View>
+        <H6 style={border}>Heading 6</H6>
+        <H6 style={border}>{'"Radical obedience to Christ is not easy... It\'s not comfort, not health, not wealth, and not prosperity in this world. Radical obedience to Christ risks losing all these things. But in the end, such risk finds its reward in Christ. And He is more than enough for us." ― David Platt'}</H6>
+      </View>
     );
   });
--- src/@ui/typography/H6/index.js
+++ src/@ui/typography/H6/index.js
@@ -1,4 +1,4 @@
-import { Text } from 'react-native';
+import { Platform, Text } from 'react-native';
 import PropTypes from 'prop-types';
 import { compose, pure, setPropTypes } from 'recompose';
 import styled from '@ui/styled';
@@ -7,8 +7,22 @@ import { withPlaceholder, Typography } from '@ui/Placeholder';
 const styles = styled(({ theme }) => ({
   fontSize: theme.helpers.rem(0.875),
   fontFamily: theme.typography.fontFamilySansNeue.bold.default,
-  lineHeight: theme.helpers.verticalRhythm(0.875, 1.02),
   color: theme.colors.text.primary,
+  ...Platform.select({
+    ios: {
+      paddingTop: theme.helpers.rem(0.125),
+      paddingBottom: theme.helpers.rem(0.1),
+      lineHeight: theme.helpers.verticalRhythm(0.875, 1.014),
+    },
+    android: {
+      lineHeight: theme.helpers.verticalRhythm(0.875, 1.214),
+    },
+    web: {
+      paddingTop: theme.helpers.rem(0.15),
+      paddingBottom: theme.helpers.rem(0.05),
+      lineHeight: theme.helpers.verticalRhythm(0.875, 1.05),
+    },
+  }),
 }), 'H6');
 
 const H6 = compose(
