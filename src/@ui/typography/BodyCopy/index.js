c14967b15e74301598d1dd61558017ebbe7312ca
Burke Shartsis <burke@burkeshartsis.com>
2018-01-30 13:43:08 -0500

    adjusting BodyCopy lineheight and padding to render consistantly across all platforms 😬

--- src/@ui/typography/BodyCopy/index.js
+++ src/@ui/typography/BodyCopy/index.js
@@ -1,14 +1,28 @@
-import { Text } from 'react-native';
+import { Platform, Text } from 'react-native';
 import PropTypes from 'prop-types';
 import { compose, pure, setPropTypes } from 'recompose';
 import styled from '@ui/styled';
 import { withPlaceholder, Typography } from '@ui/Placeholder';
 
 const styles = styled(({ theme }) => ({
-  fontSize: theme.helpers.rem(1),
-  lineHeight: theme.helpers.verticalRhythm(1, 1),
+  fontSize: theme.helpers.rem(1.112),
   fontFamily: theme.typography.fontFamilySerif.regular.default,
   color: theme.colors.text.primary,
+  ...Platform.select({
+    ios: {
+      paddingTop: theme.helpers.rem(0.1),
+      paddingBottom: theme.helpers.rem(0.125),
+      lineHeight: theme.helpers.verticalRhythm(1.112, 1.4889),
+    },
+    android: {
+      lineHeight: theme.helpers.verticalRhythm(1.112, 1.625),
+    },
+    web: {
+      paddingTop: theme.helpers.rem(0.125),
+      paddingBottom: theme.helpers.rem(0.1),
+      lineHeight: theme.helpers.verticalRhythm(1.112, 1.4889),
+    },
+  }),
 }), 'BodyCopy');
 
 const BodyCopy = compose(
