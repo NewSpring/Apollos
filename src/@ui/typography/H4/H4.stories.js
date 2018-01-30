d166c86c4a11e76efff172a8db6c87d41101e98a
Burke Shartsis <burke@burkeshartsis.com>
2018-01-30 00:54:52 -0500

    adjusting h4 lineheight and padding to render consistantly across all platforms 😬

--- src/@ui/typography/H4/H4.stories.js
+++ src/@ui/typography/H4/H4.stories.js
@@ -1,7 +1,6 @@
 import React from 'react';
-
+import { View } from 'react-native';
 import { storiesOf } from '@storybook/react-native';
-
 
 import H4 from './';
 
@@ -10,9 +9,12 @@ storiesOf('@ui/typography/H4', module)
     <H4>{'"What you are is God\'s gift to you, what you become is your gift to God." ― Hans Urs von Balthasar'}</H4>
   ))
   .add('Border Box – platform testing', () => {
-    const border = { borderWidth: 1 };
+    const border = { borderWidth: 1, borderStyle: 'solid' };
 
     return (
-      <H4 style={border}>Heading 4</H4>
+      <View>
+        <H4 style={border}>Heading 4</H4>
+        <H4 style={border}>{'"What you are is God\'s gift to you, what you become is your gift to God." ― Hans Urs von Balthasar'}</H4>
+      </View>
     );
   });
--- src/@ui/typography/H4/index.js
+++ src/@ui/typography/H4/index.js
@@ -1,4 +1,4 @@
-import { Text } from 'react-native';
+import { Platform, Text } from 'react-native';
 import PropTypes from 'prop-types';
 import { compose, pure, setPropTypes } from 'recompose';
 import styled from '@ui/styled';
@@ -7,8 +7,22 @@ import { withPlaceholder, Typography } from '@ui/Placeholder';
 const styles = styled(({ theme }) => ({
   fontSize: theme.helpers.rem(1.4),
   fontFamily: theme.typography.fontFamilySansNeue.bold.default,
-  lineHeight: theme.helpers.verticalRhythm(1.4, 1.117),
   color: theme.colors.text.primary,
+  ...Platform.select({
+    ios: {
+      paddingTop: theme.helpers.rem(0.225),
+      paddingBottom: theme.helpers.rem(0.15),
+      lineHeight: theme.helpers.verticalRhythm(1.4, 1.117),
+    },
+    android: {
+      lineHeight: theme.helpers.verticalRhythm(1.8, 1.025),
+    },
+    web: {
+      paddingTop: theme.helpers.rem(0.2),
+      paddingBottom: theme.helpers.rem(0.1),
+      lineHeight: theme.helpers.verticalRhythm(1.4, 1.145),
+    },
+  }),
 }), 'H4');
 
 const H4 = compose(
