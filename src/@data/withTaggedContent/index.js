e87b471a9623deb33339bb33773d81885ad5ff90
Marcelo Reyna <Lepozepo@gmail.com>
2018-01-31 09:14:24 -0800

    Patch withTaggedContent

--- src/@data/withTaggedContent/index.js
+++ src/@data/withTaggedContent/index.js
@@ -1,16 +1,39 @@
 import { graphql } from 'react-apollo';
-import taggedContentQuery from './taggedContentQuery';
+import gql from 'graphql-tag';
 
-export default graphql(taggedContentQuery, {
-  props: ({ data: { entries } }) => ({
+export const QUERY = gql`
+  query GetTaggedContent {
+    entries: taggedContent(
+      tagName: "giving",
+      limit: 2,
+      includeChannels: ["articles"],
+      cache: false
+    ) {
+      id
+      title
+      channelName
+      status
+      meta {
+        summary
+        urlTitle
+      }
+      content {
+        isLiked
+        images(sizes: ["large"]) {
+          fileName
+          fileType
+          fileLabel
+          url
+        }
+      }
+    }
+  }
+`;
+
+export default graphql(QUERY, {
+  props: ({ data: { entries, loading } }) => ({
     entries,
-  }),
-  options: (ownProps = {}) => ({
-    variables: {
-      tagName: ownProps.tagName,
-      limit: ownProps.limit || 2,
-      includeChannels: ownProps.includeChannels || ['articles'],
-    },
+    isLoading: loading,
   }),
 });
 
