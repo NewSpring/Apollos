b1f0ec2d095e495797a20eaf10b2624845afc078
Marcelo Reyna <Lepozepo@gmail.com>
2018-02-01 13:59:04 -0800

    Restore contributions and user

--- src/give/RestoredCheckout.js
+++ src/give/RestoredCheckout.js
@@ -1,16 +1,29 @@
 import React from 'react';
+import {
+  AsyncStorage,
+} from 'react-native';
 import get from 'lodash/get';
 import BackgroundView from '@ui/BackgroundView';
 import { UIText } from '@ui/typography';
 import Button from '@ui/Button';
 import { parse } from '@utils/queryString';
+import withGive from '@data/withGive';
 
-export default function RestoredCheckout(props) {
-  const { redirect } = parse(get(props, 'location.search', {}));
+export default withGive((props) => {
+  const { redirect, state, userToken } = parse(get(props, 'location.search', {}));
 
+  // Restore state
+  props.restoreContributions(state);
+
+  // Restore user
+  AsyncStorage.setItem('authToken', userToken);
+
+  // LEFT OFF HERE: Need to turn restoreLogic into a decorator
+  // And render PaymentConfirmation underneath it
   return (
     <BackgroundView>
       <UIText>{`${redirect}`}</UIText>
+      <UIText>{`${state}`}</UIText>
       <Button
         onPress={() => {
           window.location.href = redirect;
@@ -19,4 +32,4 @@ export default function RestoredCheckout(props) {
       />
     </BackgroundView>
   );
-}
+});
