190cd8f6bdea91411efff613d8bcd78eee89b268
Burke Shartsis <burke@burkeshartsis.com>
2018-02-01 23:23:57 -0500

    more consistant math notation

--- src/@ui/TableView/FormFields.js
+++ src/@ui/TableView/FormFields.js
@@ -3,7 +3,7 @@ import styled from '@ui/styled';
 
 const FormFields = styled(({ theme }) => ({
   paddingTop: theme.sizing.baseUnit * 0.6,
-  paddingBottom: theme.sizing.baseUnit * 0.5,
+  paddingBottom: theme.sizing.baseUnit / 2,
 }))(PaddedView);
 
 export default FormFields;
