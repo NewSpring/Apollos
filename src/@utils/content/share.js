9a69fcb5316923456195201669cad2cab45347c1
Conrad VanLandingham <hi@convan.me>
2018-02-02 09:25:20 -0600

    Add message of iOS

--- src/@utils/content/share.js
+++ src/@utils/content/share.js
@@ -5,6 +5,7 @@ import getSiteLink from './getSiteLink';
 const share = (content) => {
   Share.share({
     title: content.title || content.name,
+    message: content.title || content.name,
     url: getSiteLink(content),
   });
 };
