291a366ff19a06e6572720b4ee11c2ed9681dce0
Marcelo Reyna <Lepozepo@gmail.com>
2018-02-04 14:07:19 -0800

    264 details views (#268)
    * Remove TmpGiveNavigator
    
    * Start writing/styling give routes
    
    * Split render logic between web and native
    
    * Split logic
    
    * Make GiveRoutes a bit more modular
    
    * Patch isValid warning
    
    isInitialValid does not accept a promise
    
    * Allow grouped routes to render subroutes
    
    * Do not render if a route does not exist
    
    * Update react-native-tab to 0.0.74
    
    * Build custom TabViewPagerPan
    
    To keep the router in sync after the fact, still need to patch ios and android
    
    * Only use TabViewPagerPanPatched on web
    
    * Only pass handleOnChangeTab on web
    
    * Make sure we can reach the correct index on the header consistently
    
    * Remove silly console.log
    
    * Allow CashAmountIndicator color override
    
    * Patch chevron colors
    
    * Build HistoricalContributionCard without profile image
    
    * Patch withTransactions
    
    * Set up initial render
    
    * Remove TabViewPagerPatched
    
    * Split code
    
    * Remap data enable infinite scrolling
    
    * backtrack on how route pushing is handled when navigating to the same route`
    
    * use replace instead of push to prevent muddy history
    
    * Patch propTypes
    
    * Make TabView a context provider to easily share a `jumpTo` function
    
    * Clean up and remove context
    
    While context would allow deep usage of the prop the paradigm is to stay away from context when possible (plus this allows nested TabView's) just in case
    
    * Build a small engine to allow filtering
    
    * Start setting up filter
    
    * Move DateInput component
    
    * Set up toggle
    
    * Add text, style it a bit more
    
    * Add FileSaver for web only
    
    * Add base64toblob
    
    * Simplify getPDF
    
    * Simplify download PDF UX
    
    * Refetch transaction queries
    
    * some quick empty state tweaks for contributionhistory
    
    * clean up transaction filter styles
    
    * Wire up transaction detail
    
    * Wire up schedule details
    
    * Match before hitting route bag
    
    * Remove noise
    
    * Add web-compatible DateInput component
    
    * clean up styles
    
    * path to support clearing filters
    
    * fix duplicate key warning
    
    * update snaps
    
    * Patch withTransaction
    
    * Build unstyled transaction details
    
    * Finish styling Transaction
    
    * Put ContributionHistoryFilter inside scroll view
    
    If the filter gets clipped there's no way of reaching it
    
    * Normalize DateInput test timezone
    
    * Normalize dates
    
    * .utc() isn't working with toDate O_O
    
    * Patch test again
    
    * Add ids to query
    
    * Patch withTaggedContent
    
    * Wrap up TransactionDetails
    
    * Connect history detail
    
    * Stub out ScheduleDetails
    
    * Build CancelScheduleButton
    
    * Patch queries to update the store correctly
    
    * Flush out transaction detail styles
    
    * Fix DateInput element / style tweaks for mobile
    
    * Give schedule detail view styling
    
    * Fix tests
    
    * Update snaps
    
    * Add desktop hero images to detail views

--- src/@data/withTransaction/index.js
+++ src/@data/withTransaction/index.js
@@ -1,9 +1,54 @@
 import { graphql } from 'react-apollo';
-import transactionQuery from './transactionQuery';
+import gql from 'graphql-tag';
+import get from 'lodash/get';
 
-export default graphql(transactionQuery, {
-  props: ({ data: { content } }) => ({
-    content,
+export const QUERY = gql`
+  query GetTransaction($id: ID!) {
+    transaction: node(id: $id) {
+      id
+      ... on Transaction {
+        id
+        date
+        summary
+        status
+        person {
+          id
+          firstName
+          nickName
+          lastName
+        }
+        details {
+          id
+          amount
+          account {
+            id
+            name
+            description
+            summary
+            end
+            start
+          }
+        }
+        payment {
+          id
+          paymentType
+          accountNumber
+        }
+      }
+    }
+  }
+`;
+
+export default graphql(QUERY, {
+  props: ({ data: { transaction, loading } }) => ({
+    isLoading: loading,
+    transaction: {
+      ...transaction,
+      payment: {
+        ...get(transaction, 'payment'),
+        paymentMethod: get(transaction, 'payment.paymentType', '') === 'ACH' ? 'bankAccount' : 'creditCard',
+      },
+    },
   }),
   options: (ownProps = {}) => ({
     variables: {
