a303b3fa6797e2252b57bba1d7721f21ad2442f2
Conrad VanLandingham <conrad@differential.com>
2018-02-05 16:34:22 -0600

    style TabBar to better match current app (#266)
    * style TabBar to better match current app
    
    * update snapshots
    
    * Updated tab bar background color implementation to use theming best practices.
    
    * moving transform fix to gpu for web 💪

--- src/@ui/TabBar/index.js
+++ src/@ui/TabBar/index.js
@@ -1,8 +1,11 @@
 import { StyleSheet } from 'react-native';
-import SafeAreaView from '@ui/SafeAreaView';
 import { compose } from 'recompose';
-import { enhancer as mediaQuery } from '@ui/MediaQuery';
+import Color from 'color';
+
+import { withThemeMixin } from '@ui/theme';
 import styled from '@ui/styled';
+import { enhancer as mediaQuery } from '@ui/MediaQuery';
+import SafeAreaView from '@ui/SafeAreaView';
 
 export { default as Link } from './Link';
 export { default as Layout } from './Layout';
@@ -22,8 +25,12 @@ const styles = StyleSheet.create({
 });
 
 const TabBar = compose(
+  withThemeMixin({
+    type: 'dark',
+  }),
   styled(({ theme }) => ({
-    backgroundColor: theme.colors.darkPrimary,
+    // todo - this color was hardcoded in Holtzman and has no corresponding theme value
+    backgroundColor: Color(theme.colors.background.default).darken(0.325).hex(),
   }), 'TabBar'),
   mediaQuery(({ md }) => ({ maxWidth: md }),
     styled(styles.horizontal, 'TabBar@narrow'),
