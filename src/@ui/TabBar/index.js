c25be8659c12d4ea6fae76ba4f8e116ec59802f9
Burke Shartsis <burke@burkeshartsis.com>
2018-02-05 16:12:58 -0500

    Updated tab bar background color implementation to use theming best practices.

--- src/@ui/TabBar/index.js
+++ src/@ui/TabBar/index.js
@@ -1,8 +1,11 @@
 import { StyleSheet } from 'react-native';
-import SafeAreaView from '@ui/SafeAreaView';
 import { compose } from 'recompose';
-import { enhancer as mediaQuery } from '@ui/MediaQuery';
+import Color from 'color';
+
+import { withThemeMixin } from '@ui/theme';
 import styled from '@ui/styled';
+import { enhancer as mediaQuery } from '@ui/MediaQuery';
+import SafeAreaView from '@ui/SafeAreaView';
 
 export { default as Link } from './Link';
 export { default as Layout } from './Layout';
@@ -22,10 +25,13 @@ const styles = StyleSheet.create({
 });
 
 const TabBar = compose(
-  styled({
+  withThemeMixin({
+    type: 'dark',
+  }),
+  styled(({ theme }) => ({
     // todo - this color was hardcoded in Holtzman and has no corresponding theme value
-    backgroundColor: '#202020',
-  }, 'TabBar'),
+    backgroundColor: Color(theme.colors.background.default).darken(0.325).hex(),
+  }), 'TabBar'),
   mediaQuery(({ md }) => ({ maxWidth: md }),
     styled(styles.horizontal, 'TabBar@narrow'),
     styled(styles.vertical, 'TabBar@wide'),
