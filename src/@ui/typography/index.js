84d703538084ce426f9d43a2ad03ceb04a36a7c8
Burke Shartsis <burke@burkeshartsis.com>
2018-02-05 14:45:09 -0500

    removing depricated UIText component 🎉

--- src/@ui/typography/index.js
+++ src/@ui/typography/index.js
@@ -6,4 +6,3 @@ export { default as H5 } from './H5';
 export { default as H6 } from './H6';
 export { default as H7 } from './H7';
 export { default as BodyText } from './BodyText';
-export { default as UIText } from './UIText';
