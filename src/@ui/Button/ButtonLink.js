2051cfa452b3ae0eb17b48b10372ddbb3a19c370
Burke Shartsis <burke@burkeshartsis.com>
2018-02-06 11:33:21 -0500

    updated ButtonLink style (underline) to match Holtzman.

--- src/@ui/Button/ButtonLink.js
+++ src/@ui/Button/ButtonLink.js
@@ -3,6 +3,7 @@ import styled from '@ui/styled';
 
 const ButtonLink = styled(({ theme }) => ({
   color: theme.colors.text.link,
+  textDecorationLine: 'underline',
 }), 'Button.Link')(Text);
 
 export default ButtonLink;
