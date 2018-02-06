59bc943f308a85fd364b61c1af06eb3e05d3ce32
Marcelo Reyna <Lepozepo@gmail.com>
2018-02-06 13:55:32 -0800

    onSelectFile now expects a File

--- src/@ui/forms/UploadProfileImageForm/index.js
+++ src/@ui/forms/UploadProfileImageForm/index.js
@@ -7,21 +7,23 @@ import fetch from '@utils/fetch';
 const UploadProfileImageForm = compose(
   withUser,
   withProps(props => ({
-    onSelectFile: async (e) => {
+    onSelectFile: async (file) => {
+      // console.log({ file });
       try {
-        const { files } = e.currentTarget;
         if (!Settings.ROCK_PUBLIC_TOKEN || !Settings.ROCK_URL) throw new Error('Project is not setup to receive profile images');
 
         const data = new FormData();
-        data.append('file', files[0]);
+        data.append('file', file);
 
-        const id = await fetch(`${Settings.ROCK_URL}api/BinaryFiles/Upload?binaryFileTypeId=5`, {
+        const res = await fetch(`${Settings.ROCK_URL}api/BinaryFiles/Upload?binaryFileTypeId=5`, {
           method: 'POST',
           headers: {
             'Authorization-Token': Settings.ROCK_PUBLIC_TOKEN,
           },
           body: data,
-        }).then(res => res.json());
+        });
+        // console.log({ res });
+        const id = JSON.parse(res);
 
         await props.attachPhotoIdToUser({
           id,
@@ -29,6 +31,7 @@ const UploadProfileImageForm = compose(
 
         return true;
       } catch (err) {
+        // console.log(err);
         throw err;
       }
     },
