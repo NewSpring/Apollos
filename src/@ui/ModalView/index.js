834dd753fd353148afc5d0357bd92504f88fa3f5
Conrad VanLandingham <hi@convan.me>
2018-02-07 00:10:20 -0600

    Add ability for ModalView to close to a specified route
    Fixes backbutton

--- src/@ui/ModalView/index.js
+++ src/@ui/ModalView/index.js
@@ -1,24 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { StyleSheet } from 'react-native';
-import SafeAreaView from '@ui/SafeAreaView';
+import BackgroundView from '@ui/BackgroundView';
 import SecondaryNav from '@ui/SecondaryNav';
 import { nest } from 'recompose';
 
 import ModalContainer from './ModalContainer';
 
-const ModalView = ({ children, onBackPress }) => (
-  <SafeAreaView style={StyleSheet.absoluteFill} forceInset={{ vertical: 'always', horizontal: 'always' }}>
+const ModalView = ({
+  children, onBackPress, backTo, onBackReplace,
+}) => (
+  <BackgroundView style={StyleSheet.absoluteFill} forceInset={{ vertical: 'always', horizontal: 'always' }}>
     <ModalContainer>
       {children}
-      <SecondaryNav backButton backButtonIcon="close" onBackPress={onBackPress} />
+      <SecondaryNav backButton backButtonIcon="close" onBackPress={onBackPress} backTo={backTo} onBackReplace={onBackReplace} />
     </ModalContainer>
-  </SafeAreaView>
+  </BackgroundView>
 );
 
 ModalView.propTypes = {
   children: PropTypes.node,
   onBackPress: PropTypes.func,
+  backTo: PropTypes.string,
+  onBackReplace: PropTypes.bool,
 };
 
 // quick n' dirty asModal hoc
