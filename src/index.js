639b41111fc05a0230f90aa83135784515ef50cb
Conrad VanLandingham <hi@convan.me>
2018-02-08 17:41:23 -0600

    potentially fix sentry execution on netlify

--- src/index.js
+++ src/index.js
@@ -14,9 +14,16 @@ import orientation from '@utils/orientation';
 import AppRouter from './AppRouter';
 import SentryContext from './SentryContext';
 
+const sentryEnv = Settings.APP_SENTRY_ENVIRONMENT || Settings.NODE_ENV;
+
+// expo-sentry disables reporting unless the build was built under NODE_ENV=production.
+// However, this isn't always the case, especially on netlify. So, we use
+// APP_SENTRY_ENVIRONMENT to determine whether Sentry should run
+Sentry.enableInExpoDevelopment = sentryEnv !== 'development';
+
 Sentry.config(Settings.APP_SENTRY_URL).install({
   release: Settings.COMMIT_SHA,
-  environment: Settings.APP_SENTRY_ENVIRONMENT || Settings.NODE_ENV,
+  environment: sentryEnv,
 });
 
 const App = nest(
