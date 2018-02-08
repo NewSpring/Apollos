4e99c93b7e25c5fbee543f9424e893fb3d984440
Isaac Hardy <isaac@isaachardy.io>
2018-02-08 12:36:19 -0500

    replaced Heighliner url with env var url

--- src/@data/Client/httpLink.js
+++ src/@data/Client/httpLink.js
@@ -1,5 +1,3 @@
 import { createHttpLink } from 'apollo-link-http';
 
-// export default createHttpLink({ uri: 'https://api.newspring.cc/graphql' });
-export default createHttpLink({ uri: 'https://alpha-api.newspring.cc/graphql' });
-// export default createHttpLink({ uri: 'http://localhost:8888/graphql' });
+export default createHttpLink({ uri: process.ENV.HEIGHLINER_URL });
