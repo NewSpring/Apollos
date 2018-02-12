410b0fd572d1e6402e09988890b3d48fabf0d354
Conrad VanLandingham <hi@convan.me>
2018-02-12 14:22:49 -0600

    add status text component to use for showing error/success messages on forms

---
+++ src/@ui/forms/FormStatusText.js
@@ -0,0 +1,12 @@
+import styled from '@ui/styled';
+import { H6 } from '@ui/typography';
+
+export default styled(({ theme, error, success }) => {
+  const statusColor = {};
+  if (success) statusColor.color = theme.colors.primary;
+  if (error) statusColor.color = theme.colors.alert;
+  return ({
+    ...statusColor,
+    textAlign: 'center',
+  });
+})(H6);
