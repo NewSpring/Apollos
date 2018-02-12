012f923ba2ce503a47458fb55d27ef7f087246f0
Burke Shartsis <burke@burkeshartsis.com>
2018-02-12 14:37:12 -0500

    fixing storybook web 😬

--- config/env.js
+++ config/env.js
@@ -35,7 +35,20 @@ dotenvFiles.forEach(dotenvFile => {
   }
 });
 
-// only expose env vars that being in APP_
+/*
+ * This lets you use absolute paths in imports inside large monorepos: https://github.com/facebookincubator/create-react-app/issues/253.
+ * It works similar to `NODE_PATH` in Node itself: https://nodejs.org/api/modules.html#modules_loading_from_the_global_folders
+ * Note that unlike in Node, only *relative* paths from `NODE_PATH` are honored. Otherwise, we risk
+ * importing Node.js core modules into an app instead of Webpack shims. https://github.com/facebookincubator/create-react-app/issues/1023#issuecomment-265344421
+ * We also resolve them to make sure all tools using them work consistently.
+ */
+process.env.NODE_PATH = (process.env.NODE_PATH || '')
+  .split(path.delimiter)
+  .filter(folder => folder && !path.isAbsolute(folder))
+  .map(folder => path.resolve(appDirectory, folder))
+  .join(path.delimiter);
+
+// only expose env vars that begin with APP_
 const APP = /^APP_/i;
 
 function getClientEnvironment(publicUrl) {
