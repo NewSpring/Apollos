3f03a5ff240927b2a7e1f27c68ceb87b203e20dd
Conrad VanLandingham <hi@convan.me>
2018-02-14 09:35:44 -0600

    polish touch feedback on iOS

--- src/@ui/Touchable/index.js
+++ src/@ui/Touchable/index.js
@@ -1,5 +1,11 @@
 import { Platform, TouchableNativeFeedback, TouchableOpacity } from 'react-native';
+import { defaultProps } from 'recompose';
 
-const Touchable = Platform.OS === 'android' ? TouchableNativeFeedback : TouchableOpacity;
+const IOSTouchable = defaultProps({
+  delayPressIn: 50,
+  activeOpacity: 0.5,
+})(TouchableOpacity);
+
+const Touchable = Platform.OS === 'android' ? TouchableNativeFeedback : IOSTouchable;
 
 export default Touchable;
