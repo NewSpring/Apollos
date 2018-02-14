6ca5065391d8a8c62d9f026cbc8fc8756947755e
Conrad VanLandingham <hi@convan.me>
2018-02-14 00:42:26 -0600

    undo changes to embedded video player

--- src/@ui/EmbeddedVideoPlayer/index.js
+++ src/@ui/EmbeddedVideoPlayer/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import { compose, pure, setPropTypes } from 'recompose';
 import PropTypes from 'prop-types';
-import { TouchableWithoutFeedback } from 'react-native';
-import { withMediaPlayerActions } from '@data/mediaPlayer';
 
 import WebView from './WebView';
 
 const enhance = compose(
   pure,
-  withMediaPlayerActions,
   setPropTypes({
     src: PropTypes.string,
   }),
@@ -18,19 +15,16 @@ const buildSourceObject = source => ({ uri: source });
 
 const EmbeddedVideoPlayer = enhance(({
   src,
-  pause: pauseAppAudio,
   ...otherProps
 }) => (
-  <TouchableWithoutFeedback onPress={pauseAppAudio} style={{ flex: 1 }}>
-    <WebView
-      source={buildSourceObject(src)}
-      mediaPlaybackRequiresUserAction={false}
-      allowsInlineMediaPlayback
-      bounces={false}
-      scrollEnabled={false}
-      {...otherProps}
-    />
-  </TouchableWithoutFeedback>
+  <WebView
+    source={buildSourceObject(src)}
+    mediaPlaybackRequiresUserAction={false}
+    allowsInlineMediaPlayback
+    bounces={false}
+    scrollEnabled={false}
+    {...otherProps}
+  />
 ));
 
 export default EmbeddedVideoPlayer;
