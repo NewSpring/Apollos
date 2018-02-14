7cf20a2ec13d91fa904cc1662daa93e7370e345d
Conrad VanLandingham <hi@convan.me>
2018-02-14 00:39:58 -0600

    undo pointing to localhost heighliner

--- src/@data/Client/httpLink.js
+++ src/@data/Client/httpLink.js
@@ -1,5 +1,4 @@
 import { createHttpLink } from 'apollo-link-http';
-// import Settings from '@utils/Settings';
+import Settings from '@utils/Settings';
 
-// export default createHttpLink({ uri: Settings.APP_HEIGHLINER_URL });
-export default createHttpLink({ uri: 'http://192.168.1.40:8888/graphql' });
+export default createHttpLink({ uri: Settings.APP_HEIGHLINER_URL });
