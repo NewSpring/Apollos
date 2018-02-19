f21e24de2748adc4366c2c9c9e195e0230c264fc
Conrad VanLandingham <conrad@differential.com>
2018-02-19 14:30:24 -0600

    fix empty array error and implement basic aspect ratio sorting (#352)
    * fix empty array error and implement basic aspect ratio sorting
    
    * update snaps

--- src/@utils/content/getItemImages.js
+++ src/@utils/content/getItemImages.js
@@ -4,7 +4,17 @@ const getItemImages = (item) => {
   let images = item.photo || get(item, 'content.images', []);
   if (!images.length) images = get(item, 'parent.content.images', [{}]);
   if (typeof images === 'string') return images;
-  return images[0].url; // TODO short circuit. Highliner should handle image selection in the array
+  // Sort 1:1 images to the front of array.
+  // FWIW, `ConnectedImage` will sort out which image is best to use from a resolution
+  // perspective, which is why we just kind of blindly push 1:1 images to the front,
+  // as it doesn't consider aspect ratio, just resolution.
+  images = [].concat(images).sort((a, b) => { // make sort pure
+    if (a && b && a.fileLabel === b.fileLabel) return 0;
+    if (a && a.fileLabel === '1:1') return -1;
+    if (b && b.fileLabel === '1:1') return 1;
+    return 0;
+  });
+  return images;
 };
 
 export default getItemImages;
