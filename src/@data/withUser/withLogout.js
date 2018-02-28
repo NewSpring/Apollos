4882b374e99adb4c1d961b20ec80082977344a9e
Marcelo Reyna <Lepozepo@gmail.com>
2018-02-28 14:27:10 -0800

    Patch withLogout

--- src/@data/withUser/withLogout.js
+++ src/@data/withUser/withLogout.js
@@ -3,7 +3,6 @@ import { graphql } from 'react-apollo';
 import { AsyncStorage } from 'react-native';
 
 import clearCache from '@data/Client/clearCache';
-import { QUERY as USER_QUERY } from './withUser';
 
 export const MUTATION = gql`
   mutation logoutUser {
@@ -15,24 +14,9 @@ export default graphql(MUTATION, {
   props: ({ mutate }) => ({
     logout: async () => {
       try {
-        const r = await mutate({
-          // NOTE: No need to refetch since we're clearing the cache anyway
-          // But leaving dependencies here for reference
-          // refetchQueries: [
-          //   'GivingDashboard',
-          //   'GetCheckoutData',
-          //   'SavedPaymentMethods',
-          //   'GetTransactions',
-          // ],
-          update: async (proxy) => {
-            const query = USER_QUERY;
-            const data = proxy.readQuery({ query });
-            data.person = null;
-            await AsyncStorage.removeItem('authToken');
-            proxy.writeQuery({ query, data });
-            await clearCache();
-          },
-        });
+        const r = await mutate();
+        await AsyncStorage.removeItem('authToken');
+        await clearCache();
         return r;
       } catch (err) {
         throw err;
