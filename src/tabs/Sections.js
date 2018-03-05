377658bdd28a6f92dedb60c88604a1e610fbcc31
Burke Shartsis <burke@burkeshartsis.com>
2018-03-04 20:57:02 -0500

    final wierd flex positioning fix wherein scrolling the sections menu caused the tab bar to sit not stay at the bottom of the screen.

--- src/tabs/Sections.js
+++ src/tabs/Sections.js
@@ -9,21 +9,26 @@ import styled from '@ui/styled';
 import TileNav from '@ui/TileNav';
 import Video from '@ui/VideoPlayer';
 import withSections from '@data/withSections';
-import { ResponsiveSideBySideView, Left, Right } from '@ui/SideBySideView';
+import { Right, Left, ResponsiveSideBySideView } from '@ui/SideBySideView';
 import { H1 } from '@ui/typography';
 
+// This series of flexed positioning and mediaQuery styles is incrediblely convoluted.
+// TODO: consider refactor
 const FixedWidthMenu = styled(({ theme }) => ({
   maxWidth: theme.breakpoints.sm,
   flex: 1,
 }));
 
-const Menu = compose(mediaQuery(({ md }) => ({ minWidth: md }), FixedWidthMenu))(Left);
-
-const FlexedRight = styled({
+const Flexed = styled({
   flex: 1,
-})(Right);
+});
+
+const FlexedRight = Flexed(Right);
+
+const Menu = compose(mediaQuery(({ md }) => ({ minWidth: md }), FixedWidthMenu, Flexed))(Left);
 
 const FlexedResponsiveSideBySideView = styled({ flex: 1 })(ResponsiveSideBySideView);
+
 const BackgroundVideo = () => (
   <Video
     src="https://s3.amazonaws.com/ns.images/newspring/fpo/HomepageVideo_ForExport_V3-Web_Hero_2_000kbps.mp4"
