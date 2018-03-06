d41c55139e2257fc2c3a6ea1597c529e6656317d
Burke Shartsis <burke@burkeshartsis.com>
2018-03-06 13:35:30 -0500

    Using theme for webBackgroundThumbnail in ForgotPassword. Mild presentational ✨code✨ refactor.

--- src/auth/ForgotPassword.js
+++ src/auth/ForgotPassword.js
@@ -1,58 +1,50 @@
 import React from 'react';
+import { compose, pure, setPropTypes } from 'recompose';
 import PropTypes from 'prop-types';
-import { compose } from 'recompose';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
-import Header from '@ui/Header';
-import Hero from '@ui/Hero';
-import ForgotPasswordForm from '@ui/forms/ForgotPasswordForm';
+
 import MediaQuery, { enhancer as mediaQuery } from '@ui/MediaQuery';
-import PaddedView from '@ui/PaddedView';
-import styled from '@ui/styled';
-import Video from '@ui/VideoPlayer';
 import { asModal } from '@ui/ModalView';
-import { ResponsiveSideBySideView, Left, Right } from '@ui/SideBySideView';
 import { withTheme } from '@ui/theme';
+import styled from '@ui/styled';
+import { ResponsiveSideBySideView, Left, Right } from '@ui/SideBySideView';
+import Header from '@ui/Header';
+import PaddedView from '@ui/PaddedView';
+import ForgotPasswordForm from '@ui/forms/ForgotPasswordForm';
+import Hero from '@ui/Hero';
+import Video from '@ui/VideoPlayer';
 import { H1 } from '@ui/typography';
 
-const FixedWidthLeftSide = styled(({ theme }) => ({
+const enhance = compose(
+  pure,
+  setPropTypes({
+    webBackgroundSource: PropTypes.string,
+    webBackgroundThumbnail: PropTypes.string,
+    isModal: PropTypes.bool,
+  }),
+  mediaQuery(({ md }) => ({ maxWidth: md }), asModal),
+  withTheme(({ theme: { web: { backgroundVideo, backgroundVideoThumbnail = {} } = {} } = {} }) => ({
+    webBackgroundSource: backgroundVideo,
+    webBackgroundThumbnail: backgroundVideoThumbnail,
+  })),
+);
+
+const flexed = styled({
+  flex: 1,
+});
+const FlexedResponsiveSideBySideView = flexed(ResponsiveSideBySideView);
+const FlexedRight = flexed(Right);
+
+const fixedWidthLeftSide = styled(({ theme }) => ({
   maxWidth: theme.breakpoints.sm,
   flex: 1,
   backgroundColor: theme.colors.background.default,
 }));
-
-const Flexed = styled({
-  flex: 1,
-});
-
-const LeftSide = compose(mediaQuery(({ md }) => ({ minWidth: md }), FixedWidthLeftSide, Flexed))(
+const LeftSide = compose(mediaQuery(({ md }) => ({ minWidth: md }), fixedWidthLeftSide, flexed))(
   Left,
 );
 
-const FlexedRight = Flexed(Right);
-
-const FlexedResponsiveSideBySideView = styled({ flex: 1 })(ResponsiveSideBySideView);
-
-const BackgroundVideo = ({ src }) => (
-  <Video
-    src={src}
-    posterSource="https://dg0ddngxdz549.cloudfront.net/images/cached/images/remote/http_s3.amazonaws.com/ns.images/newspring/homepage/hero_poster_2x1_1700_850_90_c1.jpg"
-    useNativeControls={false}
-    shouldPlay
-    isLooping
-  />
-);
-BackgroundVideo.propTypes = {
-  src: PropTypes.string,
-};
-
-const enhance = compose(
-  mediaQuery(({ md }) => ({ maxWidth: md }), asModal),
-  withTheme(({ theme: { web: { backgroundVideo = {} } = {} } = {} }) => ({
-    webBackgroundSource: backgroundVideo,
-  })),
-);
-
-const ForgotPassword = enhance(({ isModal, webBackgroundSource }) => (
+const ForgotPassword = enhance(({ isModal, webBackgroundSource, webBackgroundThumbnail }) => (
   <FlexedResponsiveSideBySideView>
     <LeftSide>
       <Header titleText="Forgot Password" webEnabled />
@@ -65,7 +57,17 @@ const ForgotPassword = enhance(({ isModal, webBackgroundSource }) => (
     {isModal ? null : (
       <MediaQuery minWidth={'md'}>
         <FlexedRight>
-          <Hero background={<BackgroundVideo src={webBackgroundSource} />}>
+          <Hero
+            background={
+              <Video
+                src={webBackgroundSource}
+                posterSource={webBackgroundThumbnail}
+                useNativeControls={false}
+                shouldPlay
+                isLooping
+              />
+            }
+          >
             <H1>Welcome to NewSpring</H1>
           </Hero>
         </FlexedRight>
@@ -73,9 +75,5 @@ const ForgotPassword = enhance(({ isModal, webBackgroundSource }) => (
     )}
   </FlexedResponsiveSideBySideView>
 ));
-
-ForgotPassword.propTypes = {
-  webBackgroundSource: PropTypes.string,
-};
 
 export default ForgotPassword;
