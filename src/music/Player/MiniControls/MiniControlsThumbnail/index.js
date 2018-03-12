08b340fa530d5ae7c68513fdbc99565b87c4b431
Burke Shartsis <burke@burkeshartsis.com>
2018-03-12 10:49:19 -0400

    WIP adjusting layering, sizing, and background color. Needs opacity and probably the ability to theme aware.

--- src/music/Player/MiniControls/MiniControlsThumbnail/index.js
+++ src/music/Player/MiniControls/MiniControlsThumbnail/index.js
@@ -4,8 +4,9 @@ import PropTypes from 'prop-types';
 import { View, StyleSheet } from 'react-native';
 
 import styled from '@ui/styled';
-import ConnectedImage from '@ui/ConnectedImage';
 import Touchable from '@ui/Touchable';
+import { withTheme } from '@ui/theme';
+import ConnectedImage from '@ui/ConnectedImage';
 import Icon from '@ui/Icon';
 
 const enhance = compose(
@@ -21,17 +22,27 @@ const Wrapper = styled(({ theme }) => ({
   width: theme.sizing.baseUnit * 2,
 }))(View);
 
-const CloseButton = styled(StyleSheet.absoluteFillObject)(Touchable);
+const CloseButton = styled({
+  ...StyleSheet.absoluteFillObject,
+  zIndex: 2,
+})(Touchable);
+
+const ThemedIcon = compose(
+  withTheme(({ theme: { sizing: { baseUnit = {} } = {} } = {} }) => ({ size: baseUnit * 2 })),
+  styled({
+    backgroundColor: '#ffffff',
+  }),
+)(Icon);
 
 const Image = styled({
   aspectRatio: 1,
 })(ConnectedImage);
 
-const MiniControlsThumbnail = enhance(({ source, isPlaying }) => (
+const MiniControlsThumbnail = enhance(({ source, isPlaying = true }) => (
   <Wrapper>
     {isPlaying ? (
       <CloseButton>
-        <Icon name="close" />
+        <ThemedIcon name="close" />
       </CloseButton>
     ) : null}
     <Image source={source} />
