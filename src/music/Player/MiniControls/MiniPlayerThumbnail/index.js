35d80f5fa1b1827cff623ea82c0b19ce5c667e78
Burke Shartsis <burke@burkeshartsis.com>
2018-03-11 23:59:40 -0400

    WIP – adding close button.

--- src/music/Player/MiniControls/MiniPlayerThumbnail/index.js
+++ src/music/Player/MiniControls/MiniPlayerThumbnail/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { compose, pure, setPropTypes } from 'recompose';
 import PropTypes from 'prop-types';
+import { View, StyleSheet } from 'react-native';
 
 import styled from '@ui/styled';
 import ConnectedImage from '@ui/ConnectedImage';
+import Touchable from '@ui/Touchable';
+import Icon from '@ui/Icon';
 
 const enhance = compose(
   pure,
@@ -13,10 +16,26 @@ const enhance = compose(
   }),
 );
 
+const Wrapper = styled(({ theme }) => ({
+  height: theme.sizing.baseUnit * 2, // based on MiniControls text sizing
+  width: theme.sizing.baseUnit * 2,
+}))(View);
+
+const CloseButton = styled(StyleSheet.absoluteFillObject)(Touchable);
+
 const Image = styled({
   aspectRatio: 1,
 })(ConnectedImage);
 
-const MiniPlayerThumbnail = enhance(({ source }) => <Image source={source} />);
+const MiniPlayerThumbnail = enhance(({ source, isPlaying }) => (
+  <Wrapper>
+    {isPlaying ? (
+      <CloseButton>
+        <Icon name="close" />
+      </CloseButton>
+    ) : null}
+    <Image source={source} />
+  </Wrapper>
+));
 
 export default MiniPlayerThumbnail;
