2cf698362f424661ea620993cbeee7103323d635
Burke Shartsis <burke@burkeshartsis.com>
2018-03-27 13:18:28 -0400

    420 Content Titles (#423)
    * swapping content title from H2 to H3
    
    * updated ByLine styling to have some slightly better vertical rhythem.
    
    * updated snapshots.

--- src/@ui/typography/H6/index.js
+++ src/@ui/typography/H6/index.js
@@ -4,27 +4,30 @@ import { compose, pure, setPropTypes } from 'recompose';
 import styled from '@ui/styled';
 import { withPlaceholder, Typography } from '@ui/Placeholder';
 
-const styles = styled(({ theme }) => ({
-  fontSize: theme.helpers.rem(0.875),
-  fontFamily: theme.typography.fontFamilySans.bold.default,
-  color: theme.colors.text.primary,
-  ...Platform.select({
-    ios: {
-      paddingTop: theme.helpers.rem(0.25),
-      paddingBottom: theme.helpers.rem(0.1),
-      lineHeight: theme.helpers.verticalRhythm(0.875, 1.014),
-    },
-    android: {
-      paddingTop: theme.helpers.rem(0.125),
-      lineHeight: theme.helpers.verticalRhythm(0.875, 1.214),
-    },
-    web: {
-      paddingTop: theme.helpers.rem(0.3),
-      paddingBottom: theme.helpers.rem(0.075),
-      lineHeight: theme.helpers.verticalRhythm(0.875, 1.05),
-    },
+const styles = styled(
+  ({ theme }) => ({
+    fontSize: theme.helpers.rem(0.875),
+    fontFamily: theme.typography.fontFamilySans.bold.default,
+    color: theme.colors.text.primary,
+    ...Platform.select({
+      ios: {
+        paddingTop: theme.helpers.rem(0.25),
+        paddingBottom: theme.helpers.rem(0.1),
+        lineHeight: theme.helpers.verticalRhythm(0.875, 1.014),
+      },
+      android: {
+        paddingTop: theme.helpers.rem(0.125),
+        lineHeight: theme.helpers.verticalRhythm(0.875, 1.214),
+      },
+      web: {
+        paddingTop: theme.helpers.rem(0.3),
+        paddingBottom: theme.helpers.rem(0.075),
+        lineHeight: theme.helpers.verticalRhythm(0.875, 1.05),
+      },
+    }),
   }),
-}), 'H6');
+  'H6',
+);
 
 const H6 = compose(
   setPropTypes({
