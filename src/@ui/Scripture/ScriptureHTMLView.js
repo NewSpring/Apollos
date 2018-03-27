e2f38ce3d4eaef088eac87cedb0c5ac47d746756
Burke Shartsis <burke@burkeshartsis.com>
2018-03-27 11:23:05 -0400

    fixing block indent bug that was showing up in Song of Solomon.

--- src/@ui/Scripture/ScriptureHTMLView.js
+++ src/@ui/Scripture/ScriptureHTMLView.js
@@ -2,15 +2,15 @@ import React from 'react';
 import { withProps } from 'recompose';
 
 import HTMLView, { defaultRenderer, wrapTextChildren } from '@ui/HTMLView';
-import { Platform, Text } from 'react-native';
+import { Platform, Text, View } from 'react-native';
 import { H7, BodyText } from '@ui/typography';
 import Paragraph from '@ui/Paragraph';
 import styled from '@ui/styled';
 
-const IndentedText = styled(({ theme }) => ({
+const BlockIndent = styled(({ theme }) => ({
   paddingTop: theme.sizing.baseUnit / 2,
   paddingLeft: theme.sizing.baseUnit / 2,
-}))(Text);
+}))(View);
 
 const RedLetters = styled(({ theme }) => ({
   color: theme.colors.wordOfChrist,
@@ -26,7 +26,8 @@ const NumText = styled(({ theme }) => ({
   }),
 }))(H7);
 
-const renderer = (node, { children, ...other }) => { // eslint-disable-line
+const renderer = (node, { children, ...other }) => {
+  // eslint-disable-line
   // the defaultRenderer support several basic elements out of the box,
   // this function only needs to handle the cases that are unique to scripture.
   const className = (node && node.attribs && node.attribs.class) || '';
@@ -40,15 +41,27 @@ const renderer = (node, { children, ...other }) => { // eslint-disable-line
   }
 
   if (className.includes('line-group')) {
-    return <BodyText>{children}{'\n'}</BodyText>;
+    return (
+      <BodyText>
+        {children}
+        {'\n'}
+      </BodyText>
+    );
   }
 
-  if (className.includes('block-indent')) { // todo
-    return <IndentedText>{children}</IndentedText>;
+  if (className.includes('block-indent')) {
+    // todo
+    return <BlockIndent>{children}</BlockIndent>;
   }
 
-  if (className.includes('indent')) { // todo
-    return <Text>{'     '}{children}</Text>;
+  if (className.includes('indent')) {
+    // todo
+    return (
+      <Text>
+        {'     '}
+        {children}
+      </Text>
+    );
   }
 
   if (className.includes('small-caps')) {
@@ -59,7 +72,13 @@ const renderer = (node, { children, ...other }) => { // eslint-disable-line
     return <RedLetters>{children}</RedLetters>;
   }
 
-  if (node.name === 'p') return <Paragraph><BodyText>{wrapTextChildren(children)}</BodyText></Paragraph>;
+  if (node.name === 'p') {
+    return (
+      <Paragraph>
+        <BodyText>{wrapTextChildren(children)}</BodyText>
+      </Paragraph>
+    );
+  }
 
   return defaultRenderer(node, { children, ...other });
 };
