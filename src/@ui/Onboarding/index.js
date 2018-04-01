656ecfad4bed77d4d4fd2de2895f92da242e5742
richarddubay <richardtdubayjr@gmail.com>
2018-04-01 12:18:44 -0400

    added a NEXT button and styles for it.

--- src/@ui/Onboarding/index.js
+++ src/@ui/Onboarding/index.js
@@ -1,68 +1,91 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import PropTypes from "prop-types";
-import { compose, pure, setPropTypes } from "recompose";
-import AppIntroSlider from "react-native-app-intro-slider";
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import PropTypes from 'prop-types';
+import { compose, pure, setPropTypes } from 'recompose';
+import AppIntroSlider from 'react-native-app-intro-slider';
 
 const enhance = compose(pure, setPropTypes({}));
 
 const styles = StyleSheet.create({
+  buttonCircle: {
+    width: 80,
+    height: 40,
+    backgroundColor: 'rgba(0, 0, 0, .7)',
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#FFFFFF',
+  },
   image: {
-    width: "100%",
-    height: "100%",
+    width: '100%',
+    height: '100%',
+    position: 'absolute',
+    top: 0,
+    left: 0,
   },
 });
 
 const slides = [
   {
-    key: "one",
-    image: require("../../assets/onboarding/onboard-img1.jpg"),
+    key: 'one',
+    image: require('../../assets/onboarding/onboard-img1.jpg'),
     imageStyle: styles.image,
-    backgroundColor: "#ffffff",
+    backgroundColor: '#ffffff',
   },
   {
-    key: "two",
-    image: require("../../assets/onboarding/onboard-img2.jpg"),
+    key: 'two',
+    image: require('../../assets/onboarding/onboard-img2.jpg'),
     imageStyle: styles.image,
-    backgroundColor: "#ffffff",
+    backgroundColor: '#ffffff',
   },
   {
-    key: "three",
-    image: require("../../assets/onboarding/onboard-img3.jpg"),
+    key: 'three',
+    image: require('../../assets/onboarding/onboard-img3.jpg'),
     imageStyle: styles.image,
-    backgroundColor: "#ffffff",
+    backgroundColor: '#ffffff',
   },
   {
-    key: "four",
-    image: require("../../assets/onboarding/onboard-img4.jpg"),
+    key: 'four',
+    image: require('../../assets/onboarding/onboard-img4.jpg'),
     imageStyle: styles.image,
-    backgroundColor: "#ffffff",
+    backgroundColor: '#ffffff',
   },
   {
-    key: "five",
-    image: require("../../assets/onboarding/onboard-img5.jpg"),
+    key: 'five',
+    image: require('../../assets/onboarding/onboard-img5.jpg'),
     imageStyle: styles.image,
-    backgroundColor: "#ffffff",
+    backgroundColor: '#ffffff',
   },
   {
-    key: "six",
-    image: require("../../assets/onboarding/onboard-img6.jpg"),
+    key: 'six',
+    image: require('../../assets/onboarding/onboard-img6.jpg'),
     imageStyle: styles.image,
-    backgroundColor: "#ffffff",
+    backgroundColor: '#ffffff',
   },
   {
-    key: "seven",
-    image: require("../../assets/onboarding/onboard-img7.jpg"),
+    key: 'seven',
+    image: require('../../assets/onboarding/onboard-img7.jpg'),
     imageStyle: styles.image,
-    backgroundColor: "#ffffff",
+    backgroundColor: '#ffffff',
   },
   {
-    key: "eight",
-    image: require("../../assets/onboarding/onboard-img8.jpg"),
+    key: 'eight',
+    image: require('../../assets/onboarding/onboard-img8.jpg'),
     imageStyle: styles.image,
-    backgroundColor: "#ffffff",
+    backgroundColor: '#ffffff',
   },
 ];
-const Onboarding = enhance(() => <AppIntroSlider slides={slides} />);
+
+const renderNextButton = () => (
+  <View style={styles.buttonCircle}>
+    <Text style={styles.buttonText}>Next</Text>
+  </View>
+);
+
+const Onboarding = enhance(() => (
+  <AppIntroSlider slides={slides} renderNextButton={renderNextButton} />
+));
 
 export default Onboarding;
