e60c83b13047bfc648732d5cd1b59b38130b39cf
Burke Shartsis <burke@burkeshartsis.com>
2018-04-03 09:22:33 -0400

    Fix contact us button on group finder
    fix wherein we were double nesting touchables by way of default props on Link and Button. Fixed by passing one as the component to the other 🕺💥 (#452)

--- src/group-finder/AdUnit.js
+++ src/group-finder/AdUnit.js
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import Card, { CardContent } from '@ui/Card';
 import { H4 } from '@ui/typography';
 import { Link } from '@ui/NativeWebRouter';
@@ -6,17 +7,20 @@ import Button from '@ui/Button';
 import styled from '@ui/styled';
 import PaddedView from '@ui/PaddedView';
 
-const Text = styled(({ textAlign: 'center' }))(H4);
+const Text = styled({ textAlign: 'center' })(H4);
 
 const AdUnit = () => (
   <Card>
     <CardContent>
       <PaddedView>
-        <Text>{'Can\'t find what you\'re looking for, or want to start your own group?'}</Text>
+        <Text>{"Can't find what you're looking for, or want to start your own group?"}</Text>
       </PaddedView>
-      <Link to="https://rock.newspring.cc/workflows/81">
-        <Button bordered title="Contact us" />
-      </Link>
+      <Link
+        bordered
+        title="Contact us"
+        component={Button}
+        to="https://rock.newspring.cc/workflows/81"
+      />
     </CardContent>
   </Card>
 );
