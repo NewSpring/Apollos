099dc62f6edb6cf7ca69135f5e62f6646a8eeba3
Isaac Hardy <isaac@isaachardy.io>
2018-04-04 16:18:23 -0400

    removed rogue logs

--- src/@data/Client/errorLink.js
+++ src/@data/Client/errorLink.js
@@ -1,25 +1,16 @@
 import { onError } from 'apollo-link-error';
 import sentry from '@utils/sentry';
 
-const link = onError(({
-  graphQLErrors,
-  networkError,
-}) => {
+const link = onError(({ graphQLErrors, networkError }) => {
   if (graphQLErrors) {
-    graphQLErrors.forEach(({
-      message,
-      locations,
-      path,
-    }) => {
+    graphQLErrors.forEach(({ message, locations, path }) => {
       const msg = `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`;
-      console.error(msg);
       sentry.captureException(msg);
     });
   }
 
   if (networkError) {
     const msg = `[Network error]: ${networkError}`;
-    console.error(msg);
     sentry.captureException(msg);
   }
 });
