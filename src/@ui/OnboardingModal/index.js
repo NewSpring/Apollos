95ed86d36ea3abe57f6ac6c07f2e6ef1b21148d1
richarddubay <richardtdubayjr@gmail.com>
2018-04-04 11:46:15 -0400

    correctly passing a boolean to the visible prop of the modal

--- src/@ui/OnboardingModal/index.js
+++ src/@ui/OnboardingModal/index.js
@@ -1,40 +1,65 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import { AsyncStorage, Modal } from 'react-native';
-import { compose, pure, setPropTypes } from 'recompose';
+// import { compose, pure, setPropTypes } from 'recompose';
 import Onboarding from '../Onboarding';
 
-const enhance = compose(pure, setPropTypes({}));
+// const enhance = compose(pure, setPropTypes({}));
 
-const showModal = async () => {
-  try {
-    // Read from AsyncStorage (https://facebook.github.io/react-native/docs/asyncstorage.html).
-    const value = await AsyncStorage.getItem('onboarded');
-    if (value === null) {
-      // There is no value in AsyncStorage for this. Show the onboarding modal.
-      return true;
+class OnboardingModal extends PureComponent {
+  state = {
+    showModal: false,
+  };
+
+  componentDidMount() {
+    this.showModal();
+  }
+
+  closeModal = async () => {
+    // Write the value to AsyncStorage and close the modal.
+    console.log('closeModal');
+    try {
+      await AsyncStorage.setItem('onboarded', 'flibbertygibbet');
+    } catch (error) {
+      // Error saving data
+      console.log("Couldn't save the onboarded data to AsyncStorage", error);
     }
-  } catch (error) {
-    // Error retrieving data
-    console.log("Couldn't read the onboarding data from AsyncStorage", error);
-  }
-  return false;
-};
+    console.log('onboarded = ', await AsyncStorage.getItem('onboarded'));
+    this.showModal();
+  };
 
-const closeModal = async () => {
-  // Write the value to AsyncStorage and close the modal.
-  try {
-    await AsyncStorage.setItem('onboarded', true);
-  } catch (error) {
-    // Error saving data
-    console.log("Couldn't save the onboarded data to AsyncStorage", error);
-  }
-  showModal();
-};
+  showModal = async () => {
+    console.log('showModal');
+    try {
+      // Read from AsyncStorage (https://facebook.github.io/react-native/docs/asyncstorage.html).
+      const value = await AsyncStorage.getItem('onboarded');
+      console.log('value = ', value);
+      if (value === null) {
+        // There is no value in AsyncStorage for this. Show the onboarding modal.
+        this.setState({ showModal: true });
+        return true;
+      }
+    } catch (error) {
+      // Error retrieving data
+      console.log("Couldn't read the onboarding data from AsyncStorage", error);
+    }
+    // await AsyncStorage.removeItem('onboarded');
+    this.setState({ showModal: false });
+    return false;
+  };
 
-const OnboardingModal = enhance(() => (
-  <Modal visible={showModal} onDismiss={closeModal} animationType="slide">
-    <Onboarding closeModal={closeModal} />
-  </Modal>
-));
+  render() {
+    return (
+      <Modal visible={this.state.showModal} onDismiss={this.closeModal} animationType="slide">
+        <Onboarding closeModal={this.closeModal} />
+      </Modal>
+    );
+  }
+}
+
+// const OnboardingModal = enhance(() => (
+//   <Modal visible={showModal} onDismiss={closeModal} animationType="slide">
+//     <Onboarding closeModal={closeModal} />
+//   </Modal>
+// ));
 
 export default OnboardingModal;
