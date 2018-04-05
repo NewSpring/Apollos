45a5d99bb40ee08b2f48e6db4bc2ef31ae330efd
Conrad VanLandingham <conrad@differential.com>
2018-04-05 14:18:57 -0700

    Force the display of 1:1 images if available (#466)

--- src/@ui/ContentView/Media/ImageHeader.js
+++ src/@ui/ContentView/Media/ImageHeader.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 
 import GradientOverlayImage from '@ui/GradientOverlayImage';
 import ProgressiveImage from '@ui/ProgressiveImage';
+import { getItemImages } from '@utils/content';
 
 const enhance = compose(
   pure,
@@ -17,7 +18,8 @@ const enhance = compose(
 const ImageHeader = enhance(({ images, thumbnail, imageOverlayColor }) => (
   <GradientOverlayImage
     ImageComponent={ProgressiveImage}
-    source={images}
+    // todo: calling getItemImages like this is kinda dirty
+    source={getItemImages({ content: { images } })}
     thumbnail={thumbnail}
     overlayColor={imageOverlayColor}
   />
