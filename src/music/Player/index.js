5b92f4111f79ef163215da4b46fab24e61bf349f
Conrad VanLandingham <hi@convan.me>
2018-04-12 15:11:36 -0500

    Revert "TEMP: remove audio player to test back button functionality"
    This reverts commit f0e2643dbc624c6364072dfaf2feea75ff1a6a37.

--- src/music/Player/index.js
+++ src/music/Player/index.js
@@ -198,25 +198,24 @@ export class DockableMediaPlayer extends PureComponent {
   );
 
   render() {
-    return this.props.children;
-    // return (
-    //   <Audio
-    //     source={this.props.currentTrack && this.props.currentTrack.file}
-    //     isPlaying={this.props.isPlaying}
-    //     onPlaybackReachedEnd={this.handleEndReached}
-    //     style={StyleSheet.absoluteFill}
-    //   >
-    //     <CardStack direction="vertical">
-    //       <Route exact path={'/player'} render={this.renderPlayer} />
-    //       <Route exact path={'/player/list/:id'} component={asModal(Playlist)} />
-    //       <Route exact path={'/player/:id/:track'} component={PlayerTrackContextual} />
-    //       <BackgroundView>
-    //         {this.props.children}
-    //         {this.renderMiniControls()}
-    //       </BackgroundView>
-    //     </CardStack>
-    //   </Audio>
-    // );
+    return (
+      <Audio
+        source={this.props.currentTrack && this.props.currentTrack.file}
+        isPlaying={this.props.isPlaying}
+        onPlaybackReachedEnd={this.handleEndReached}
+        style={StyleSheet.absoluteFill}
+      >
+        <CardStack direction="vertical">
+          <Route exact path={'/player'} render={this.renderPlayer} />
+          <Route exact path={'/player/list/:id'} component={asModal(Playlist)} />
+          <Route exact path={'/player/:id/:track'} component={PlayerTrackContextual} />
+          <BackgroundView>
+            {this.props.children}
+            {this.renderMiniControls()}
+          </BackgroundView>
+        </CardStack>
+      </Audio>
+    );
   }
 }
 
