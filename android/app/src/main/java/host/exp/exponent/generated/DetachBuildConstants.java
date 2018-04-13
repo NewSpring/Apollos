c717f4e0ba82a8dd4558b9b2d4bb8b591507d7b9
Burke Shartsis <burke@burkeshartsis.com>
2018-04-13 14:35:34 -0400

    adding generated file that has been removed and added multiple times in favor of using --skip-worktree locally

---
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -0,0 +1,8 @@
+package host.exp.exponent.generated;
+
+// This file is auto-generated. Please don't rename!
+public class DetachBuildConstants {
+
+  public static final String DEVELOPMENT_URL = "expba3ad9e96a6043fc95726c73cb381a27://localhost:19000";
+
+}
