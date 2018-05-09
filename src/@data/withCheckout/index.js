2279a3946cba1f8ed19751bbbba831e7823b19af
richarddubay <richardtdubayjr@gmail.com>
2018-05-09 15:00:36 -0400

    return the id number of the state

--- src/@data/withCheckout/index.js
+++ src/@data/withCheckout/index.js
@@ -10,25 +10,20 @@ export default graphql(checkoutQuery, {
   },
   props({ ownProps, data }) {
     const {
-      campuses,
-      countries,
-      states,
-      person,
-      savedPaymentMethods,
-      loading,
+      campuses, countries, states, person, savedPaymentMethods, loading,
     } = data;
 
-    return ({
+    return {
       isLoading: ownProps.isLoading || loading,
       campuses: campuses || [],
       countries: (countries || []).map(c => ({ label: c.description, id: c.value })),
-      states: (states || []).map(s => ({ label: s.description, id: s.value })),
+      states: (states || []).map(s => ({ label: s.description, id: s.value, idNumber: s._id })), // eslint-disable-line
       person,
       savedPaymentMethods: (savedPaymentMethods || []).map(pm => ({
         ...pm,
         paymentMethod: pm.payment.paymentType === 'ACH' ? 'bankAccount' : 'creditCard',
         accountNumber: pm.payment.accountNumber,
       })),
-    });
+    };
   },
 });
