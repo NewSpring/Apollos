75d05d142dbb7f594bbd1752429902cce9fbc2b1
Richard Dubay <richardtdubayjr@gmail.com>
2018-05-18 13:33:09 -0400

    Correct font style on Thumbnail Cards (#648)
    * fix children not displaying as serif on thumbnail cards
    
    * updated snapshots
    
    * check for children before displaying them
    
    * updated ThumbnailCard to better handle and display descriptions. Refactored unneccsary code away for a more declaritive API.

--- src/@ui/ThumbnailCard/ThumbnailCard.tests.js
+++ src/@ui/ThumbnailCard/ThumbnailCard.tests.js
@@ -8,8 +8,19 @@ describe('the ThumbnailCard component', () => {
   it('should render', () => {
     const tree = renderer.create(
       <Providers>
+        <ThumbnailCard title={'Why Jesus is Timeless'} />
+      </Providers>,
+    );
+    expect(tree).toMatchSnapshot();
+  });
+  it('should render with a description', () => {
+    const tree = renderer.create(
+      <Providers>
         <ThumbnailCard
           title={'Why Jesus is Timeless'}
+          description={
+            'This is the story of a God, who made a flood and drowned the whole world. He gave up His son for everybody, and we absolutely love Him, all the time.'
+          }
         />
       </Providers>,
     );
@@ -29,10 +40,7 @@ describe('the ThumbnailCard component', () => {
   it('should render with a category', () => {
     const tree = renderer.create(
       <Providers>
-        <ThumbnailCard
-          title={'Why Jesus is Timeless'}
-          category={'Time Travel'}
-        />
+        <ThumbnailCard title={'Why Jesus is Timeless'} category={'Time Travel'} />
       </Providers>,
     );
     expect(tree).toMatchSnapshot();
