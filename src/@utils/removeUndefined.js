b1475384f92c00c20b49b77e60e9931eed1d5bc6
richarddubay <richardtdubayjr@gmail.com>
2018-06-20 14:54:05 -0400

    put removeUndefined back the way it was

--- src/@utils/removeUndefined.js
+++ src/@utils/removeUndefined.js
@@ -1,13 +1,6 @@
 // Removes keys with undefined values from an object
 // module({ thing: undefined, data: 0 }) => { data: 0 }
-import sentry from '@utils/sentry';
 
 export default function (obj) {
-  let returnValue = {};
-  try {
-    returnValue = JSON.parse(JSON.stringify(obj));
-  } catch (e) {
-    sentry.captureException(e);
-  }
-  return returnValue;
+  return JSON.parse(JSON.stringify(obj));
 }
