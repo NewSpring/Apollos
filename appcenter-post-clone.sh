5c99ee85bca7bac0792cad53c581a945d7607ae9
Isaac Hardy <isaac.hardy@newspring.cc>
2018-09-14 10:30:59 -0400

    add exp cli

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -4,3 +4,4 @@ brew uninstall node@6
 NODE_VERSION="8.9.4"
 curl "https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}.pkg" > "$HOME/Downloads/node-installer.pkg"
 sudo installer -store -pkg "$HOME/Downloads/node-installer.pkg" -target "/"
+npm install -g expo-cli && exp path
