74b924e63f8f4a9bf6da989bc79c76f7274d06be
Daniel Edwards <dan.edwards@newspring.cc>
2018-10-04 11:43:57 -0400

    Removed Identify from Login

--- src/@data/withUser/withLogin.js
+++ src/@data/withUser/withLogin.js
@@ -2,7 +2,7 @@ import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
 import { AsyncStorage } from 'react-native';
 import get from 'lodash/get';
-import { track, events, categories, identify } from '@utils/analytics';
+import { track, events, categories } from '@utils/analytics';
 
 import { QUERY as LOGGED_IN_QUERY } from './withIsLoggedIn';
 
