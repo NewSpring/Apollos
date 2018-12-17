e4c767ba4c5f90ac21cbc94661d971cda960efb6
Michael Neeley <micneeley14@gmail.com>
2018-12-17 17:11:54 -0500

    fixed package.json one

--- scripts/bump_version.py
+++ scripts/bump_version.py
@@ -46,7 +46,7 @@ if __name__ == "__main__":
     
     # write over JS files
     _replaceLine("./app-config.js", "version:", "    version: '" + version + "',") 
-    _replaceLine("./package.json", "version:", "  \"version\": \"" + version + "\",") 
+    _replaceLine("./package.json", "version\":", "  \"version\": \"" + version + "\",") 
     
     # write over Android files
     _replaceLine("./android/app/build.gradle", "versionName", "    versionName '" + version + "'")
