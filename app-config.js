65f625aa201d4efa1c484266a4a4c295e9a79989
Michael Neeley <micneeley14@gmail.com>
2018-12-18 21:00:33 -0500

    removed android block

--- app-config.js
+++ app-config.js
@@ -4,37 +4,6 @@ module.exports = env => ({
     name: 'NewSpring',
     slug: 'newspring',
     icon: 'src/assets/icons/ios/icon-1024x1024.png',
-    ios: {
-      bundleIdentifier: 'com.subsplashstudio31.NewSpring-Church',
-      buildNumber: '1',
-      supportsTablet: true,
-      associatedDomains: [
-        'applinks:beta.newspring.cc',
-        'applinks:newspring.cc',
-        'applinks:beta-my.newspring.cc',
-        'applinks:my.newspring.cc',
-        'applinks:rm2y5.app.goo.gl',
-      ],
-      icon: 'src/assets/icons/ios/icon-1024x1024.png',
-      splash: {
-        image: 'src/assets/splash/splash-1242x2436.png',
-        backgroundColor: '#D1DCE6',
-      },
-      loadJSInBackgroundExperimental: true,
-    },
-    android: {
-      package: 'cc.newspring.newspringapp',
-      versionCode: 600025,
-      icon: 'src/assets/icons/android/icon-192x192-xxxhpdi.png',
-      splash: {
-        mdpi: 'src/assets/splash/splash-1242x2436.png',
-        hdpi: 'src/assets/splash/splash-1242x2436.png',
-        xhdpi: 'src/assets/splash/splash-1242x2436.png',
-        xxhdpi: 'src/assets/splash/splash-1242x2436.png',
-        xxxhdpi: 'src/assets/splash/splash-1242x2436.png',
-        backgroundColor: '#D1DCE6',
-      },
-    },
     hooks: {
       postPublish: [
         {
