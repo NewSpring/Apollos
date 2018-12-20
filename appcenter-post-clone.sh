c7b18b74cbda1d8ac2c11dc60921719347e185f5
Michael Neeley <micneeley14@gmail.com>
2018-12-19 22:41:52 -0500

    added fake plist file

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -6,11 +6,13 @@ curl "https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}.pkg" > "$HO
 sudo installer -store -pkg "$HOME/Downloads/node-installer.pkg" -target "/"
 #npm install -g exp && exp path
 
-# comment out expo stuff in build processes
-#
 # iOS
+# comment out calls to expo
 sed -i .bak 's/PATH=\\"$PATH:$value\\" exp prepare-detached-build --platform ios/\#PATH=\\"$PATH:$value\\" exp prepare-detached-build --platform ios/g' ios/newspring.xcodeproj/project.pbxproj
 sed -i .bak 's/value=$(cat ~\/.expo\/PATH)/\#value=$(cat ~\/.expo\/PATH)/g' ios/newspring.xcodeproj/project.pbxproj
+
+# add fake expo files as needed by the Xcode build process
+mv ios/newspring/Supporting/EXBuildConstants.plist.bak ios/newspring/Supporting/EXBuildConstants.plist
 
 # Android
 sed -i .bak 's/preBuild.dependsOn exponentPrebuildStep/\/\/preBuild.dependsOn exponentPrebuildStep/g' android/app/build.gradle
