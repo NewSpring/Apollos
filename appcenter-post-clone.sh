732108a9464d324c4d0c2553e7bf7c6b45899566
Michael Neeley <micneeley14@gmail.com>
2018-12-19 21:49:03 -0500

    updated post-clone to replace lines

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -5,3 +5,12 @@ NODE_VERSION="8.9.4"
 curl "https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}.pkg" > "$HOME/Downloads/node-installer.pkg"
 sudo installer -store -pkg "$HOME/Downloads/node-installer.pkg" -target "/"
 #npm install -g exp && exp path
+
+# comment out expo stuff in build processes
+#
+# iOS
+sed -i -e 's/PATH=\\"$PATH:$value\\" exp prepare-detached-build --platform ios/\#PATH=\\"$PATH:$value\\" exp prepare-detached-build --platform ios/g' ios/newspring.xcodeproj/project.pbxproj
+sed -i -e 's/value=$(cat ~\/.expo\/PATH)/\#value=$(cat ~\/.expo\/PATH)/g' ios/newspring.xcodeproj/project.pbxproj
+
+# Android
+sed -i -e 's/preBuild.dependsOn exponentPrebuildStep/\/\/preBuild.dependsOn exponentPrebuildStep/g'
