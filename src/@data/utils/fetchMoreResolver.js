6a4592b64fbbcbd92e39471238c3097e9d66c156
Isaac Hardy <isaac.hardy@newspring.cc>
2019-01-08 10:42:15 -0500

    simplified fetchMoreResolver Co-authored-by: Michael micneeley14@gmail.com

--- src/@data/utils/fetchMoreResolver.js
+++ src/@data/utils/fetchMoreResolver.js
@@ -1,41 +1,23 @@
 import get from 'lodash/get';
 import set from 'lodash/fp/set';
-import isEqual from 'lodash/isEqual';
 
-// NOTE: This can be improved by creating a class
-const runningRefetches = [];
-function isRefetching(props) {
-  return runningRefetches.find(refetchProps => isEqual(props, refetchProps));
-}
 
-function registerRefetch(props) {
-  runningRefetches.push(props);
-}
-
-function markRefetchAsComplete(props) {
-  const refetchIndex = runningRefetches.findIndex(refetchProps => isEqual(props, refetchProps));
-  runningRefetches.splice(refetchIndex, 1);
-}
-
-// TODO: Test this
 export default function fetchMoreResolver({ collectionName, mapTo, data } = {}) {
+  let isRefetching;
+
   return () => {
     const skip = (get(data, collectionName) || []).length;
-    const refetchProps = {
-      collectionName,
-      mapTo,
-      data,
-      skip,
-    };
+
     // Short-circuit the refetch if it's already running
-    if (isRefetching(refetchProps)) return;
-    registerRefetch(refetchProps);
+    if (isRefetching) return;
+    isRefetching = true;
 
     data.fetchMore({
       variables: { ...data.variables, skip },
       updateQuery: (previousResult, { fetchMoreResult }) => {
-        markRefetchAsComplete(refetchProps);
+        isRefetching = false;
         if (!fetchMoreResult) return previousResult;
+
         return set(mapTo || collectionName,
           [
             ...(get(previousResult, collectionName) || []),
