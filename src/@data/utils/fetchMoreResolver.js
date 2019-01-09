d9ac6c00d8c5f7f985af1cfad6db89d699510807
Michael Neeley <micneeley14@gmail.com>
2019-01-09 12:44:15 -0500

    updated some comments

--- src/@data/utils/fetchMoreResolver.js
+++ src/@data/utils/fetchMoreResolver.js
@@ -2,48 +2,35 @@ import get from 'lodash/get';
 import set from 'lodash/fp/set';
 
 // TODO: this should be passed in and managed via state
-console.log('defining fetchLock');
 let fetchLock;
 
 export default function fetchMoreResolver({
   collectionName,
-  // TODO: not sure what this is doing
+  // NOTE: not sure if mapTo is ever used
   // mapTo,
   data,
 } = {}) {
-  const i = Date.now();
-
   return () => {
-    console.log(`${i} calling fetchMore(${collectionName})`);
-    console.log(`${i} fetchLock`, fetchLock);
     // Short-circuit the fetchMore if it's already running
     if (fetchLock) {
-      console.log(`${i} short circuiting fetchMore...`);
       return;
     }
-    fetchLock = true;
-    console.log(`${i} fetchLock ON`);
 
+    // lock to make sure this function doesn't run again until this thread is done
+    fetchLock = true;
+
+    // set the new offset to the number of items in the list
     const skip = (get(data, collectionName) || []).length;
-    console.log(`${i} skipping ${skip} items...`);
 
     data.fetchMore({
       variables: { ...data.variables, skip },
       updateQuery: (previousResult, { fetchMoreResult }) => {
         // NOTE: not sure what this is doing
-        // fetchLock = false;
-        // console.log('fetchLock OFF');
-        // if (!fetchMoreResult) {
-        // console.log('returning previous data...');
-        // console.log(previousResult);
-        // return previousResult;
-        // }
-        console.log(`${i} previousResult`, previousResult);
-        console.log(`${i} fetchMoreResult`, fetchMoreResult);
+        // if (!fetchMoreResult) return previousResult;
 
         // combine previous data and fetchMore data
         const newResult = set(
-          // NOTE: not sure what this is doing
+          // NOTE: not sure if mapTo is ever used
           // mapTo || collectionName,
           collectionName,
           [
@@ -52,9 +39,8 @@ export default function fetchMoreResolver({
           ],
           { ...previousResult },
         );
-        console.log(`${i} newResult`, newResult);
-        // NOTE: the only problem with this may be if another call gets
-        // made in between the unlock
+        // TODO: the only problem with this may be if another call gets
+        // made in between the unlock and the return. Not sure how to fix.
         fetchLock = false;
         return newResult;
       },
