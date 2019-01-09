bcc42878ad505f1d5ad8507a624dc14a486820bb
Michael Neeley <micneeley14@gmail.com>
2019-01-09 17:02:03 -0500

    initialized string old fashioned way for compatibility

--- src/@utils/last4.js
+++ src/@utils/last4.js
@@ -1,3 +1,6 @@
-export default function (str = '') {
-  return str.slice(str.length - 4, str.length);
+export default function (str) {
+  // needs to be initialized the old way for compatibility
+  let safeStr = str;
+  if (safeStr === null) safeStr = '';
+  return safeStr.slice(safeStr.length - 4, safeStr.length);
 }
