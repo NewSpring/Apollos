fbaa89de83c53fa3fc4832038d170c5640291873
Michael Neeley <micneeley14@gmail.com>
2019-01-09 11:48:04 -0500

    rewrote fetchMore using semaphore

--- src/@data/utils/fetchMoreResolver.js
+++ src/@data/utils/fetchMoreResolver.js
@@ -1,30 +1,62 @@
 import get from 'lodash/get';
 import set from 'lodash/fp/set';
 
+// TODO: this should be passed in and managed via state
+console.log('defining fetchLock');
+let fetchLock;
 
-export default function fetchMoreResolver({ collectionName, mapTo, data } = {}) {
-  let isRefetching;
+export default function fetchMoreResolver({
+  collectionName,
+  // TODO: not sure what this is doing
+  // mapTo,
+  data,
+} = {}) {
+  const i = Date.now();
 
   return () => {
-    const skip = (get(data, collectionName) || []).length;
+    console.log(`${i} calling fetchMore(${collectionName})`);
+    console.log(`${i} fetchLock`, fetchLock);
+    // Short-circuit the fetchMore if it's already running
+    if (fetchLock) {
+      console.log(`${i} short circuiting fetchMore...`);
+      return;
+    }
+    fetchLock = true;
+    console.log(`${i} fetchLock ON`);
 
-    // Short-circuit the refetch if it's already running
-    if (isRefetching) return;
-    isRefetching = true;
+    const skip = (get(data, collectionName) || []).length;
+    console.log(`${i} skipping ${skip} items...`);
 
     data.fetchMore({
       variables: { ...data.variables, skip },
       updateQuery: (previousResult, { fetchMoreResult }) => {
-        isRefetching = false;
-        if (!fetchMoreResult) return previousResult;
+        // NOTE: not sure what this is doing
+        // fetchLock = false;
+        // console.log('fetchLock OFF');
+        // if (!fetchMoreResult) {
+        // console.log('returning previous data...');
+        // console.log(previousResult);
+        // return previousResult;
+        // }
+        console.log(`${i} previousResult`, previousResult);
+        console.log(`${i} fetchMoreResult`, fetchMoreResult);
 
-        return set(mapTo || collectionName,
+        // combine previous data and fetchMore data
+        const newResult = set(
+          // NOTE: not sure what this is doing
+          // mapTo || collectionName,
+          collectionName,
           [
             ...(get(previousResult, collectionName) || []),
             ...(get(fetchMoreResult, collectionName) || []),
           ],
           { ...previousResult },
         );
+        console.log(`${i} newResult`, newResult);
+        // NOTE: the only problem with this may be if another call gets
+        // made in between the unlock
+        fetchLock = false;
+        return newResult;
       },
     });
   };
