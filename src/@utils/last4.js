3410bf948eb27c07d7c8adc40ed2b358c15b16b6
Michael Neeley <micneeley14@gmail.com>
2019-01-13 13:28:11 -0500

    changed to OR statement

--- src/@utils/last4.js
+++ src/@utils/last4.js
@@ -1,6 +1,5 @@
 export default function (str = '') {
   // even though it's initialized, it can still be passed in as null
-  let safeStr = str;
-  if (safeStr === null) safeStr = '';
+  const safeStr = str || '';
   return safeStr.slice(safeStr.length - 4, safeStr.length);
 }
