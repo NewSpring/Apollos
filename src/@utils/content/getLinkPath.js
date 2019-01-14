5a78894c69fcbc970d995050a731b6a503d875e6
Isaac Hardy <isaac.hardy@newspring.cc>
2019-01-14 14:13:04 -0500

    updated url to be title and not id

--- src/@utils/content/getLinkPath.js
+++ src/@utils/content/getLinkPath.js
@@ -3,6 +3,7 @@ import { get } from 'lodash';
 const getLinkPath = (contentItem) => {
   if (!contentItem || typeof contentItem !== 'object') return '#';
   const id = contentItem.id || contentItem.id;
+  const title = contentItem.title || '';
   const category = contentItem.channelName;
   const seriesId = contentItem.parent && (contentItem.parent.id || contentItem.parent.id);
 
@@ -12,25 +13,25 @@ const getLinkPath = (contentItem) => {
 
   switch (category) {
     case 'series_newspring':
-      return `/series/${id}`;
+      return `/series/${title || id}`;
     case 'sermons':
-      return `/series/${seriesId}/sermon/${id}`;
+      return `/series/${seriesId}/sermon/${title || id}`;
     case 'studies':
-      return `/studies/${id}`;
+      return `/studies/${title || id}`;
     case 'study_entries':
-      return `/studies/${seriesId}/entry/${id}`;
+      return `/studies/${seriesId}/entry/${title || id}`;
     case 'devotionals':
-      return `/devotions/${id}`;
+      return `/devotions/${title || id || id}`;
     case 'newspring_albums':
-      return `/music/${id}`;
+      return `/music/${title || id}`;
     case 'articles':
-      return `/articles/${id}`;
+      return `/articles/${title || id}`;
     case 'stories':
-      return `/stories/${id}`;
+      return `/stories/${title || id}`;
     case 'news':
-      return `/news/${id}`;
+      return `/news/${title || id}`;
     case 'newspring_now':
-      return `/events/${id}`;
+      return `/events/${title || id}`;
     default:
       return get(contentItem, 'meta.urlTitle', '#');
   }
