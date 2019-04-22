332c44fd1e55239dcee2d2f3d5f5876f537ec5b6
Michael Neeley <micneeley14@gmail.com>
2019-04-22 16:42:57 -0400

    fixed bug color

--- src/@utils/content/getItemBgColor.js
+++ src/@utils/content/getItemBgColor.js
@@ -1,10 +1,15 @@
 import { get } from 'lodash';
+import Color from 'color';
 
 export const getItemBgColor = (item) => {
   let color = get(item, 'content.colors[0].value');
   if (!color) color = get(item, 'parent.content.colors[0].value');
   if (!color) return null;
-  return `#${color}`;
+  try {
+    return Color(`#${color}`).string();
+  } catch (e) {
+    return null;
+  }
 };
 
 export default getItemBgColor;
