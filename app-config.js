af38b887420a95af1202bebc7755838dee21e11f
Michael Neeley <micneeley14@gmail.com>
2019-05-07 17:04:26 -0400

    put some stuff back

--- app-config.js
+++ app-config.js
@@ -2,7 +2,10 @@ module.exports = env => ({
   expo: {
     sdkVersion: '30.0.0',
     name: 'NewSpring',
+    description: 'The official NewSpring mobile app.',
     slug: 'newspring',
+    primaryColor: '#6bac43',
+    icon: 'src/assets/icons/ios/icon-1024x1024.png',
     hooks: {
       postPublish: [
         {
@@ -21,10 +24,6 @@ module.exports = env => ({
     isDetached: true,
     detach: {
       scheme: 'expba3ad9e96a6043fc95726c73cb381a27',
-      iosExpoViewUrl:
-        'https://s3.amazonaws.com/exp-exponent-view-code/ios-v2.3.3-sdk25.0.0-e7a4361a-631d-4caf-9692-1880d71c394a.tar.gz',
-      androidExpoViewUrl:
-        'https://s3.amazonaws.com/exp-exponent-view-code/android-v2.3.0-sdk25.0.0-0c32461b-a7c6-4ddc-b85b-32a80c4f18d9.tar.gz',
     },
   },
 });
